c7221e6cc4b74d70050417040a2b578018ff1d18
Balazs Vass <balazs.vass@zalando.de>
2015-09-07 17:07:03 +0200

    PRICING-481 enable Swagger & Swagger UI

---
+++ src/main/java/de/zalando/bazaar/lab/Application.java
@@ -0,0 +1,11 @@
+package de.zalando.bazaar.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/de/zalando/bazaar/lab/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package de.zalando.bazaar.lab.config;
+
+import static com.google.common.base.Predicates.or;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.google.common.base.Predicate;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+
+import springfox.documentation.service.ApiInfo;
+
+import springfox.documentation.spi.DocumentationType;
+
+import springfox.documentation.spring.web.plugins.Docket;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@EnableAutoConfiguration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).groupName("api").apiInfo(apiInfo()).select().paths(apiPaths())
+                                                      .build();
+    }
+
+    private Predicate<String> apiPaths() {
+        return or(regex("/api/echo.*"));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Bazaar prototype")
+                                   .description("Prototype for next gen technologies for Zalando - Bazaar team.")
+                                   .contact("springfox").license("Apache License Version 2.0")
+                                   .licenseUrl("https://github.com/springfox/springfox/blob/master/LICENSE")
+                                   .version("2.0").build();
+    }
+}
