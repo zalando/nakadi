9dee0653ab204abdc5fa78b6b9dd75172468271e
Vyacheslav Stepanov <vyacheslav.stepanov@zalando.de>
2015-10-19 16:48:47 +0200

    added travis-ci config file; added dummy test

---
+++ src/test_dummy.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class TestDummy(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_1_plus_1(self):
+        assert 1 + 1 == 2
+
+    def test_2_plus_2(self):
+        assert 2 + 2 == 4
