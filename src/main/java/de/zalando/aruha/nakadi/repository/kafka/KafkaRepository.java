8c294b91541d3ba8c97cf67cf13f748e89b0797c
John Kliff Jochens <john.jochens@zalando.de>
2015-11-29 20:39:58 +0100

    remove some indirection

--- src/main/java/de/zalando/aruha/nakadi/repository/kafka/KafkaRepository.java
+++ src/main/java/de/zalando/aruha/nakadi/repository/kafka/KafkaRepository.java
@@ -1,58 +1,82 @@
 package de.zalando.aruha.nakadi.repository.kafka;
 
 import java.io.IOException;
-
 import java.util.List;
+import java.util.Properties;
 import java.util.stream.Collectors;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooKeeper;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import de.zalando.aruha.nakadi.domain.Topic;
 import de.zalando.aruha.nakadi.repository.TopicRepository;
-import de.zalando.aruha.nakadi.repository.zookeeper.ZooKeeperFactory;
+import de.zalando.aruha.nakadi.repository.zookeeper.ZooKeeperHolder;
 
 @Component
 public class KafkaRepository implements TopicRepository {
 
-    private static final Logger LOG = LoggerFactory.getLogger(KafkaRepository.class);
+	private static final Logger LOG = LoggerFactory.getLogger(KafkaRepository.class);
 
-    @Autowired
-    private KafkaClientFactory factory;
-    @Autowired
-    private ZooKeeperFactory zkFactory;
+	@Autowired
+	private Factory factory;
+	@Autowired
+	private ZooKeeperHolder zkFactory;
 
-    @Override
-    public List<Topic> listTopics() {
-        LOG.info("list topics");
-        try {
-            final ZooKeeper zk = zkFactory.get();
+	@Override
+	public List<Topic> listTopics() {
+		LOG.info("list topics");
+		try {
+			final ZooKeeper zk = zkFactory.get();
 
-            final List<Topic> children = zk.getChildren("/brokers/topics", false).stream().map(s -> new Topic(s))
-            		.collect(Collectors.toList());
-            LOG.info("topics: {}", children);
-            return children;
-        } catch (KeeperException | InterruptedException | IOException | NullPointerException e) {
-            LOG.error("Failed to list topics", e);
-        }
+			final List<Topic> children = zk.getChildren("/brokers/topics", false).stream().map(s -> new Topic(s))
+					.collect(Collectors.toList());
+			LOG.info("topics: {}", children);
+			return children;
+		} catch (KeeperException | InterruptedException | IOException | NullPointerException e) {
+			LOG.error("Failed to list topics", e);
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    @Override
-    public void postMessage(final String topicId, final String partitionId, final String v) {
-        LOG.info("%s %s %s", topicId, partitionId, v);
+	@Override
+	public void postMessage(final String topicId, final String partitionId, final String v) {
+		LOG.info("%s %s %s", topicId, partitionId, v);
 
-        final ProducerRecord record = new ProducerRecord<String, String>(topicId, partitionId, v);
-        factory.createProducer().send(record);
-    }
+		final ProducerRecord record = new ProducerRecord<String, String>(topicId, partitionId, v);
+		factory.createProducer().send(record);
+	}
+}
+
+@Component
+@PropertySource("${nakadi.config}")
+class Factory {
+
+	@Value("${nakadi.kafka.broker}")
+	private String kafkaAddress;
+
+	@Value("${nakadi.zookeeper.brokers}")
+	private String zookeeperAddress;
+
+	public Factory() {
+	}
+
+	public Producer createProducer() {
+
+		final Properties props = new Properties();
+		props.put("metadata.broker.list", zookeeperAddress);
+
+		final Producer<KafkaDefaults.KeySerializer, KafkaDefaults.ValueSerializer> p = new KafkaProducer<>(props);
+
+		return p;
+	}
 }
--- src/main/java/de/zalando/aruha/nakadi/repository/zookeeper/ZooKeeperHolder.java
+++ src/main/java/de/zalando/aruha/nakadi/repository/zookeeper/ZooKeeperHolder.java
@@ -2,6 +2,8 @@ package de.zalando.aruha.nakadi.repository.zookeeper;
 
 import java.io.IOException;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.zookeeper.ZooKeeper;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -12,12 +14,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 @PropertySource("${nakadi.config}")
-public class ZooKeeperFactory {
-    @Value("${nakadi.zookeeper.brokers}")
-    private String brokers;
+public class ZooKeeperHolder {
+	@Value("${nakadi.zookeeper.brokers}")
+	private String brokers;
+	private ZooKeeper zooKeeper;
 
-    public ZooKeeper get() throws IOException {
-        return new ZooKeeper(brokers, 30000, null);
-    }
+	@PostConstruct
+	public void init() throws IOException {
+		zooKeeper = new ZooKeeper(brokers, 30000, null);
+	}
 
+	public ZooKeeper get() throws IOException {
+		return zooKeeper;
+	}
 }
