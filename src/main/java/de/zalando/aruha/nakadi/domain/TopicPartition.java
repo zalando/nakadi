b9bc6f8ed45d117ded21e648f213548af4f62caf
John Kliff Jochens <john.jochens@zalando.de>
2015-11-30 00:08:03 +0100

    fix listing of partitions

--- src/main/java/de/zalando/aruha/nakadi/domain/TopicPartition.java
+++ src/main/java/de/zalando/aruha/nakadi/domain/TopicPartition.java
@@ -4,22 +4,23 @@ public class TopicPartition {
 	private String topicId;
 	private String partitionId;
 
-	public TopicPartition() {
+	public TopicPartition(final String topicId, final String partitionId) {
+		setTopicId(topicId);
+		setPartitionId(partitionId);
+	}
 
-	}
-	public TopicPartition(String topicId, String partitionId) {
-		topicId = topicId;
-		partitionId = partitionId;
-	}
 	public String getTopicId() {
 		return topicId;
 	}
+
 	public void setTopicId(final String topicId) {
 		this.topicId = topicId;
 	}
+
 	public String getPartitionId() {
 		return partitionId;
 	}
+
 	public void setPartitionId(final String partitionId) {
 		this.partitionId = partitionId;
 	}
