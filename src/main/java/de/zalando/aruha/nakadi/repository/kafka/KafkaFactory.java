e8c2fb09b277e654ab115b16b57ea21726d2335e
Vyacheslav Stepanov <vyacheslav.stepanov@zalando.de>
2015-12-08 18:15:58 +0100

    ARUHA-32 fix after merge from master

--- src/main/java/de/zalando/aruha/nakadi/repository/kafka/KafkaFactory.java
+++ src/main/java/de/zalando/aruha/nakadi/repository/kafka/KafkaFactory.java
@@ -7,9 +7,7 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.util.Properties;
