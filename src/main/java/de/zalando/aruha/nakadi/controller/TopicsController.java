0a242e35a74c2e27faf93a806974d4ec6d00b1c0
Vyacheslav Stepanov <vyacheslav.stepanov@zalando.de>
2015-12-21 15:06:06 +0100

    optimized imports

--- src/main/java/de/zalando/aruha/nakadi/controller/TopicsController.java
+++ src/main/java/de/zalando/aruha/nakadi/controller/TopicsController.java
@@ -14,8 +14,6 @@ import de.zalando.aruha.nakadi.utils.FlushableGZIPOutputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.task.TaskExecutor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -36,7 +34,6 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
