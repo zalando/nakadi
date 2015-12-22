ae051cd448023df8020cba6166f3675151d23e1e
Vyacheslav Stepanov <vyacheslav.stepanov@zalando.de>
2015-12-22 16:58:13 +0100

    ARUHA-32 finished first Acceptance Test for streaming

--- src/acceptance-test/java/de/zalando/aruha/nakadi/webservice/EventStreamReadingAT.java
+++ src/acceptance-test/java/de/zalando/aruha/nakadi/webservice/EventStreamReadingAT.java
@@ -1,47 +1,107 @@
 package de.zalando.aruha.nakadi.webservice;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableMap;
 import com.jayway.restassured.RestAssured;
-import de.zalando.aruha.nakadi.Application;
-import de.zalando.aruha.nakadi.domain.TopicPartition;
-import de.zalando.aruha.nakadi.webservice.utils.ATUtils;
+import com.jayway.restassured.response.Response;
+import de.zalando.aruha.nakadi.webservice.utils.TestHelper;
 import org.apache.http.HttpStatus;
+import org.hamcrest.collection.IsCollectionWithSize;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static com.jayway.restassured.RestAssured.given;
-import static de.zalando.aruha.nakadi.webservice.utils.ATUtils.getLatestOffsets;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertNotNull;
+import static java.text.MessageFormat.format;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasKey;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 public class EventStreamReadingAT {
 
-    private static final String TEST_TOPIC = "test-topic";
+    private static final int PORT = 8080;
+    private static final String TOPIC = "test-topic";
     private static final String PARTITION = "0";
-    private static final String EVENTS_ENDPOINT = "/topics/" + TEST_TOPIC + "/partition/" + PARTITION + "/events";
+    private static final String STREAM_ENDPOINT = format("/topics/{0}/partitions/{1}/events", TOPIC, PARTITION);
+    private static final String SEPARATOR = "\n";
+
+    private TestHelper testHelper;
+    private ObjectMapper jsonMapper = new ObjectMapper();
+    private List<Map<String, String>> initialOffsets;
 
     @Before
     public void setUp() {
-        RestAssured.port = 8080;
+        RestAssured.port = PORT;
+        testHelper = new TestHelper("http://localhost:" + PORT);
 
-//        RestAssured.given().when().post(EVENTS_ENDPOINT);
+        // grab the offset we had initially so that we know where to start reading from
+        initialOffsets = testHelper.getLatestOffsets(TOPIC);
+
+        // push some events so that we have something to stream
+        testHelper.pushEventsToPartition(TOPIC, PARTITION, "\"Dummy\"", 25);
     }
 
-    @Test
+    @Test(timeout = 10000)
     public void whenGetSingleBatchThenOk() {
-        final List latestOffsets = getLatestOffsets(TEST_TOPIC);
-        assertNotNull(latestOffsets);
-//        given()
-//                .param("start_from", "0")
-//        .when()
-//                .get(EVENTS_ENDPOINT)
-//        .then()
-//                .statusCode(HttpStatus.SC_OK)
-//                .body(is(echo));
+        // ACT //
+        final String startOffset = testHelper.getOffsetForPartition(initialOffsets, PARTITION).orElse("0");
+        final Response response = given()
+                .param("start_from", startOffset)
+                .param("batch_limit", "100")
+                .param("stream_timeout", "1")
+                .when()
+                .get(STREAM_ENDPOINT);
+
+        // ASSERT //
+        response.then().statusCode(HttpStatus.SC_OK);
+        assertStreamResponse(response.print(), 1, 25);
     }
+
+    @SuppressWarnings("unchecked")
+    private void assertStreamResponse(final String body, final int batchesCount, final int eventsInBatch) {
+        // deserialize the response body
+        final List<Map<String, Object>> batches = Arrays
+                .stream(body.split(SEPARATOR + SEPARATOR))
+                .flatMap(multiBatch -> Arrays.stream(multiBatch.split(SEPARATOR)))
+                .map(batch -> {
+                    try {
+                        return jsonMapper.<Map<String, Object>>readValue(batch, new TypeReference<HashMap<String, Object>>() {
+                        });
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        fail("Could not deserialize stream response");
+                        return ImmutableMap.<String, Object>of();
+                    }
+                })
+                .collect(Collectors.toList());
+
+        // check size
+        assertThat(batches, new IsCollectionWithSize<>(equalTo(batchesCount)));
+
+        // check staructure and content of each batch
+        batches.forEach(batch -> {
+            assertThat(batch, hasKey("cursor"));
+            final Map<String, String> cursor = (Map<String, String>) batch.get("cursor");
+
+            assertThat(cursor, hasKey("partition"));
+            assertThat(cursor.get("partition"), equalTo(PARTITION));
+            assertThat(cursor, hasKey("offset"));
+
+            assertThat(batch, hasKey("events"));
+            final List<String> events = (List<String>) batch.get("events");
+
+            assertThat(events, new IsCollectionWithSize<>(equalTo(eventsInBatch)));
+            events.forEach(event -> assertThat(event, equalTo("Dummy")));
+        });
+    }
+
 }
--- src/main/java/de/zalando/aruha/nakadi/repository/kafka/KafkaLocationConfig.java
+++ src/main/java/de/zalando/aruha/nakadi/repository/kafka/KafkaLocationConfig.java
@@ -1,6 +1,5 @@
 package de.zalando.aruha.nakadi.repository.kafka;
 
-import com.google.common.base.Strings;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,38 +15,14 @@ public class KafkaLocationConfig {
     @Value("${nakadi.zookeeper.brokers}")
     private String zookeeperAddress;
 
-    // in a case if we run nakadi in a docker container with a linked kafka docker
-    // container - we should get kafka and zk adresses from env variables set up by docker
-    @Value("#{environment['LOCAL_KAFKA_PORT_9092_TCP_ADDR']}")
-    private String linkedContainerKafkaAddress;
-
-    @Value("#{environment['LOCAL_KAFKA_PORT_9092_TCP_PORT']}")
-    private String linkedContainerKafkaPort;
-
-    @Value("#{environment['LOCAL_KAFKA_PORT_2181_TCP_ADDR']}")
-    private String linkedContainerZookeeperAddress;
-
-    @Value("#{environment['LOCAL_KAFKA_PORT_2181_TCP_PORT']}")
-    private String linkedContainerZookeeperPort;
-
     @Bean(name = "kafkaBrokers")
     public String kafkaBrokers() {
-        if (Strings.isNullOrEmpty(linkedContainerKafkaAddress)) {
-            return kafkaAddress;
-        }
-        else {
-            return linkedContainerKafkaAddress + ":" + linkedContainerKafkaPort;
-        }
+        return kafkaAddress;
     }
 
     @Bean(name = "zookeeperBrokers")
     public String zookeeperBrokers() {
-        if (Strings.isNullOrEmpty(linkedContainerZookeeperAddress)) {
-            return zookeeperAddress;
-        }
-        else {
-            return linkedContainerZookeeperAddress + ":" + linkedContainerZookeeperPort;
-        }
+        return zookeeperAddress;
     }
 
 }
