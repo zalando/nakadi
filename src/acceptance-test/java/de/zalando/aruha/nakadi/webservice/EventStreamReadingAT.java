0e4ac2fb186db0d56a5ca3bb2cba8ce5ea489d25
Vyacheslav Stepanov <vyacheslav.stepanov@zalando.de>
2015-12-22 17:41:54 +0100

    ARUHA-32 created few more tests for streaming endpoint; refactoring

--- src/acceptance-test/java/de/zalando/aruha/nakadi/webservice/EventStreamReadingAT.java
+++ src/acceptance-test/java/de/zalando/aruha/nakadi/webservice/EventStreamReadingAT.java
@@ -4,15 +4,16 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
 import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.parsing.Parser;
 import com.jayway.restassured.response.Response;
 import de.zalando.aruha.nakadi.webservice.utils.TestHelper;
 import org.apache.http.HttpStatus;
 import org.hamcrest.collection.IsCollectionWithSize;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -31,31 +32,35 @@ public class EventStreamReadingAT {
     private static final int PORT = 8080;
     private static final String TOPIC = "test-topic";
     private static final String PARTITION = "0";
-    private static final String STREAM_ENDPOINT = format("/topics/{0}/partitions/{1}/events", TOPIC, PARTITION);
+    private static final String STREAM_ENDPOINT = createStreamEndpointUrl(TOPIC, PARTITION);
     private static final String SEPARATOR = "\n";
+    public static final String DUMMY_EVENT = "Dummy";
 
     private TestHelper testHelper;
     private ObjectMapper jsonMapper = new ObjectMapper();
     private List<Map<String, String>> initialOffsets;
+    private String initialPartitionOffset;
 
-    @Before
+    @BeforeClass
     public void setUp() {
         RestAssured.port = PORT;
+        RestAssured.defaultParser = Parser.JSON;
         testHelper = new TestHelper("http://localhost:" + PORT);
 
-        // grab the offset we had initially so that we know where to start reading from
+        // grab the offsets we had initially so that we know where to start reading from
         initialOffsets = testHelper.getLatestOffsets(TOPIC);
+        initialPartitionOffset = testHelper.getOffsetForPartition(initialOffsets, PARTITION).orElse("0");
 
         // push some events so that we have something to stream
-        testHelper.pushEventsToPartition(TOPIC, PARTITION, "\"Dummy\"", 25);
+        final String event = format("\"{0}\"", DUMMY_EVENT);
+        testHelper.pushEventsToPartition(TOPIC, PARTITION, event, 20);
     }
 
     @Test(timeout = 10000)
-    public void whenGetSingleBatchThenOk() {
+    public void whenGetSingleBatchFromSinglePartitionThenOk() {
         // ACT //
-        final String startOffset = testHelper.getOffsetForPartition(initialOffsets, PARTITION).orElse("0");
         final Response response = given()
-                .param("start_from", startOffset)
+                .param("start_from", initialPartitionOffset)
                 .param("batch_limit", "100")
                 .param("stream_timeout", "1")
                 .when()
@@ -63,11 +68,45 @@ public class EventStreamReadingAT {
 
         // ASSERT //
         response.then().statusCode(HttpStatus.SC_OK);
-        assertStreamResponse(response.print(), 1, 25);
+        validateStreamResponse(response.print(), 1, 20, DUMMY_EVENT);
     }
 
-    @SuppressWarnings("unchecked")
-    private void assertStreamResponse(final String body, final int batchesCount, final int eventsInBatch) {
+    @Test(timeout = 10000)
+    public void whenGetMultipleBatchesFromSinglePartitionThenOk() {
+        // ACT //
+        final Response response = given()
+                .param("start_from", initialPartitionOffset)
+                .param("batch_limit", "5")
+                .param("stream_timeout", "1")
+                .when()
+                .get(STREAM_ENDPOINT);
+
+        // ASSERT //
+        response.then().statusCode(HttpStatus.SC_OK);
+        validateStreamResponse(response.print(), 4, 5, DUMMY_EVENT);
+    }
+
+    @Test(timeout = 10000)
+    public void whenGetEventsWithUknownTopicThenTopicNotFoundk() {
+        // ACT //
+        given()
+                .param("start_from", initialPartitionOffset)
+                .param("batch_limit", "5")
+                .param("stream_timeout", "1")
+                .when()
+                .get(createStreamEndpointUrl("blah-topic", PARTITION))
+
+        // ASSERT //
+                .then()
+                .statusCode(HttpStatus.SC_NOT_FOUND)
+                .body("message", equalTo("topic not found"));
+    }
+
+    private static String createStreamEndpointUrl(final String topic, final String partition) {
+        return format("/topics/{0}/partitions/{1}/events", topic, partition);
+    }
+
+    private void validateStreamResponse(final String body, final int batchesCount, final int eventsInBatch, final String event) {
         // deserialize the response body
         final List<Map<String, Object>> batches = Arrays
                 .stream(body.split(SEPARATOR + SEPARATOR))
@@ -88,20 +127,23 @@ public class EventStreamReadingAT {
         assertThat(batches, new IsCollectionWithSize<>(equalTo(batchesCount)));
 
         // check staructure and content of each batch
-        batches.forEach(batch -> {
-            assertThat(batch, hasKey("cursor"));
-            final Map<String, String> cursor = (Map<String, String>) batch.get("cursor");
+        batches.forEach(batch -> validateBatch(batch, eventsInBatch, event));
+    }
 
-            assertThat(cursor, hasKey("partition"));
-            assertThat(cursor.get("partition"), equalTo(PARTITION));
-            assertThat(cursor, hasKey("offset"));
+    @SuppressWarnings("unchecked")
+    private void validateBatch(final Map<String, Object> batch, final int eventsInBatch, final String expectedEvent) {
+        assertThat(batch, hasKey("cursor"));
+        final Map<String, String> cursor = (Map<String, String>) batch.get("cursor");
 
-            assertThat(batch, hasKey("events"));
-            final List<String> events = (List<String>) batch.get("events");
+        assertThat(cursor, hasKey("partition"));
+        assertThat(cursor.get("partition"), equalTo(PARTITION));
+        assertThat(cursor, hasKey("offset"));
 
-            assertThat(events, new IsCollectionWithSize<>(equalTo(eventsInBatch)));
-            events.forEach(event -> assertThat(event, equalTo("Dummy")));
-        });
+        assertThat(batch, hasKey("events"));
+        final List<String> events = (List<String>) batch.get("events");
+
+        assertThat(events, new IsCollectionWithSize<>(equalTo(eventsInBatch)));
+        events.forEach(event -> assertThat(event, equalTo(expectedEvent)));
     }
 
 }
