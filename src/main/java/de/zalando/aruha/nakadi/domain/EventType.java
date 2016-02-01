8d2df1cfff15cc78f4a7963fa3c999012ff239cd
Ricardo De Cillo <rdecillo@zalando-08707.corp.ad.zalando.net>
2016-02-01 10:36:33 +0100

    Remove unnecessary json annotations

--- src/main/java/de/zalando/aruha/nakadi/domain/EventType.java
+++ src/main/java/de/zalando/aruha/nakadi/domain/EventType.java
@@ -1,61 +1,54 @@
 package de.zalando.aruha.nakadi.domain;
 
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.collect.Lists;
+
+import java.util.List;
 
 public class EventType {
 
     private String name;
+
+    @JsonProperty("owning_application")
     private String owningApplication;
+
     private String type;
+
+    @JsonIgnore
     private final List<ValidationStrategyConfiguration> validationStrategies = Lists.newArrayList();
+
+    @JsonProperty("schema")
     private EventTypeSchema eventTypeSchema;
 
-    @JsonGetter("name")
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
 
-    @JsonSetter("name")
-    public void setName(final String name) {
-        this.name = name;
-    }
+    public void setName(final String name) { this.name = name; }
 
-    @JsonGetter("owning_application")
     public String getOwningApplication() {
         return owningApplication;
     }
 
-    @JsonSetter("owning_application")
     public void setOwningApplication(String owningApplication) {
         this.owningApplication = owningApplication;
     }
 
-    @JsonGetter("type")
     public String getType() {
         return type;
     }
 
-    @JsonSetter("type")
     public void setType(String type) {
         this.type = type;
     }
 
-    @JsonIgnore
     public List<ValidationStrategyConfiguration> getValidationStrategies() {
         return validationStrategies;
     }
 
-    @JsonGetter("schema")
     public EventTypeSchema getEventTypeSchema() {
         return eventTypeSchema;
     }
 
-    @JsonSetter("schema")
     public void setEventTypeSchema(final EventTypeSchema eventTypeSchema) {
         this.eventTypeSchema = eventTypeSchema;
     }
--- src/main/java/de/zalando/aruha/nakadi/domain/EventTypeSchema.java
+++ src/main/java/de/zalando/aruha/nakadi/domain/EventTypeSchema.java
@@ -1,7 +1,5 @@
 package de.zalando.aruha.nakadi.domain;
 
-import com.fasterxml.jackson.annotation.JsonGetter;
-import com.fasterxml.jackson.annotation.JsonSetter;
 import org.json.JSONObject;
 
 public class EventTypeSchema {
@@ -13,22 +11,18 @@ public class EventTypeSchema {
     private Type type;
     private JSONObject schema;
 
-    @JsonGetter("type")
     public Type getType() {
         return type;
     }
 
-    @JsonSetter("type")
     public void setType(final Type type) {
         this.type = type;
     }
 
-    @JsonGetter("schema")
     public JSONObject getSchema() {
         return schema;
     }
 
-    @JsonSetter("schema")
     public void setSchema(final JSONObject schema) {
         this.schema = schema;
     }
