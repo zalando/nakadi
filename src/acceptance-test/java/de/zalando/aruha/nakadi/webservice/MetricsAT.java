d08db74eaed547870fb12c143d2dbdebb57842ce
Tobias Mühl <tobias.muehl@zalando.de>
2016-02-04 15:27:22 +0100

    ARUHA-77 The /topic endpoint does not exist anymore the /event-type/.../partitions endpoint is not available, yet. Meanwhile disable this test.

--- src/acceptance-test/java/de/zalando/aruha/nakadi/webservice/MetricsAT.java
+++ src/acceptance-test/java/de/zalando/aruha/nakadi/webservice/MetricsAT.java
@@ -1,6 +1,7 @@
 package de.zalando.aruha.nakadi.webservice;
 
 import org.apache.http.HttpStatus;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static com.jayway.restassured.RestAssured.get;
@@ -12,21 +13,13 @@ import static org.hamcrest.Matchers.notNullValue;
 public class MetricsAT extends BaseAT {
 
     @Test(timeout = 5000)
-     public void whenGetMetricsThenStructureIsOk() {
-        given()
-                .when()
-                .get("/metrics")
-                .then()
-                .statusCode(HttpStatus.SC_OK)
-                .and()
-                .body("counters", notNullValue())
-                .body("gauges", notNullValue())
-                .body("histograms", notNullValue())
-                .body("meters", notNullValue())
-                .body("timers", notNullValue())
-                .body("version", notNullValue());
+    public void whenGetMetricsThenStructureIsOk() {
+        given().when().get("/metrics").then().statusCode(HttpStatus.SC_OK).and().body("counters", notNullValue())
+               .body("gauges", notNullValue()).body("histograms", notNullValue()).body("meters", notNullValue())
+               .body("timers", notNullValue()).body("version", notNullValue());
     }
 
+    @Ignore("Ignore until the new partion resource in the /event-types endpoint is available")
     @Test(timeout = 10000)
     public void whenCallEndpointThenCountInMetricsIncreased() {
 
@@ -35,13 +28,8 @@ public class MetricsAT extends BaseAT {
 
         get("/topics");
 
-        given()
-                .when()
-                .get("/metrics")
-                .then()
-                .statusCode(HttpStatus.SC_OK)
-                .and()
-                .body("timers.get_topics.count", equalTo(getTopicsCount + 1));
+        given().when().get("/metrics").then().statusCode(HttpStatus.SC_OK).and().body("timers.get_topics.count",
+            equalTo(getTopicsCount + 1));
     }
 
 }
