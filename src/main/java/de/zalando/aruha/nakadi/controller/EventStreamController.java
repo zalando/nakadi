d0cf712c037c60aad73b96133c43bf0ec23f783a
Vyacheslav Stepanov <vyacheslav.stepanov@zalando.de>
2016-02-05 16:20:41 +0100

    ARUHA-78: removed flow id;

--- src/main/java/de/zalando/aruha/nakadi/controller/EventStreamController.java
+++ src/main/java/de/zalando/aruha/nakadi/controller/EventStreamController.java
@@ -63,11 +63,8 @@ public class EventStreamController {
             @RequestParam(value = "batch_flush_timeout", required = false, defaultValue = "30") final int batchTimeout,
             @RequestParam(value = "stream_timeout", required = false, defaultValue = "0") final int streamTimeout,
             @RequestParam(value = "stream_keep_alive_limit", required = false, defaultValue = "0") final int streamKeepAliveLimit,
-            @Nullable @RequestHeader(name = "X-Flow-Id", required = false) final String flowId,
             @Nullable @RequestHeader(name = "X-nakadi-cursors", required = false) final String cursorsStr,
             final NativeWebRequest request, final HttpServletResponse response) throws IOException {
-
-        LOG.trace("starting event stream for flow id: {}", flowId);
 
         return outputStream -> {
             try {
