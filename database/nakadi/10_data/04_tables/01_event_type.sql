91def626267b00c8c7adc56de06fdd8128a72b19
Alexander Kukushkin <alexander.kukushkin@zalando.de>
2016-02-08 16:25:46 +0100

    create index on et_event_type_object

--- database/nakadi/10_data/04_tables/01_event_type.sql
+++ database/nakadi/10_data/04_tables/01_event_type.sql
@@ -3,3 +3,5 @@ CREATE TABLE IF NOT EXISTS zn_data.event_type (
   et_event_type_object jsonb NOT NULL,
   CHECK (et_event_type_object->>'name' = et_name)
 );
+
+CREATE INDEX ON zn_data.event_type USING gin (et_event_type_object);
