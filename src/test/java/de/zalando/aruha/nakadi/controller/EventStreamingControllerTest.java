d0e417db30ee797734be0b2d63817d5242991e5e
Vyacheslav Stepanov <vyacheslav.stepanov@zalando.de>
2016-02-09 15:26:14 +0100

    ARUHA-78: removed powermock

--- src/test/java/de/zalando/aruha/nakadi/controller/EventStreamingControllerTest.java
+++ src/test/java/de/zalando/aruha/nakadi/controller/EventStreamingControllerTest.java
@@ -1,18 +1,10 @@
 package de.zalando.aruha.nakadi.controller;
 
-import de.zalando.aruha.nakadi.service.EventStreamConfig;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(EventStreamConfig.class)
 public class EventStreamingControllerTest {
 
     @Test
     public void ggg() throws Exception {
-        PowerMockito.whenNew(EventStreamConfig.class).withAnyArguments().thenReturn(null);
     }
 }
