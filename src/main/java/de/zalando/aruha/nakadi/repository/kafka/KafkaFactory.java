8b61d5b11e62bd1707d346a22ad01ce11350437e
Vyacheslav Stepanov <vyacheslav.stepanov@zalando.de>
2016-02-15 16:17:38 +0100

    ARUHA-95: removed simple consumer creation as we don't need it any more

--- src/main/java/de/zalando/aruha/nakadi/repository/kafka/KafkaFactory.java
+++ src/main/java/de/zalando/aruha/nakadi/repository/kafka/KafkaFactory.java
@@ -5,9 +5,6 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 
-import de.zalando.aruha.nakadi.repository.kafka.KafkaLocationManager.Broker;
-import kafka.javaapi.consumer.SimpleConsumer;
-
 public class KafkaFactory {
     private final KafkaLocationManager kafkaLocationManager;
     private final KafkaProducer<String, String> kafkaProducer;
@@ -25,10 +22,4 @@ public class KafkaFactory {
         return new KafkaConsumer<>(kafkaLocationManager.getKafkaProperties());
     }
 
-    public SimpleConsumer getSimpleConsumer() {
-        for (Broker kafkaBroker : kafkaLocationManager.getKafkaBrokers()) {
-            return new SimpleConsumer(kafkaBroker.host, kafkaBroker.port, 1000, 64 * 1024, "leaderlookup");
-        }
-        return null;
-    }
 }
