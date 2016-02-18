f7ec91445c8fc836845ee17b43c5832dc38b9a27
Vyacheslav Stepanov <vyacheslav.stepanov@zalando.de>
2016-02-18 18:07:49 +0100

    ARUHA-56: added check of oauth scopes; introduced 3 security modes;

--- src/main/java/de/zalando/aruha/nakadi/config/SecurityConfiguration.java
+++ src/main/java/de/zalando/aruha/nakadi/config/SecurityConfiguration.java
@@ -18,12 +18,27 @@ import org.springframework.security.oauth2.provider.token.ResourceServerTokenSer
 
 import org.zalando.stups.oauth2.spring.server.TokenInfoResourceServerTokenServices;
 
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.PUT;
+
 @EnableResourceServer
 @Configuration
 public class SecurityConfiguration extends ResourceServerConfigurerAdapter {
 
     private static final Logger LOG = LoggerFactory.getLogger(SecurityConfiguration.class);
+
     public static final String UID = "uid";
+    public static final String NAKADI_READ_SCOPE = "nakadi.read";
+    public static final String EVENT_TYPE_WRITE_SCOPE = "nakadi_event_type.write";
+    public static final String EVENT_STREAM_READ_SCOPE = "nakadi_event_stream.read";
+    public static final String EVENT_STREAM_WRITE_SCOPE = "nakadi_event_stream.write";
+
+    public enum AuthMode {
+        OFF,
+        BASIC,
+        FULL
+    }
 
     @Value("${nakadi.oauth2.tokenInfoUri}")
     private String tokenInfoUri;
@@ -31,16 +46,34 @@ public class SecurityConfiguration extends ResourceServerConfigurerAdapter {
     @Value("${nakadi.oauth2.clientId}")
     private String clientId;
 
-    @Value("${nakadi.oauth2.enabled:true}")
-    private boolean oauthEnabled;
+    @Value("${nakadi.oauth2.mode:BASIC}")
+    private AuthMode authMode;
 
     @Override
     public void configure(final HttpSecurity http) throws Exception {
-        LOG.info("Security: OAuth2 is " + (oauthEnabled ? "enabled" : "disabled"));
-        if (oauthEnabled) {
-            http.authorizeRequests().antMatchers("/health").permitAll().anyRequest().access(hasScope(UID));
-        } else {
-            http.authorizeRequests().anyRequest().permitAll();
+        LOG.info("Authentication mode: " + authMode);
+        if (authMode == AuthMode.FULL) {
+            http.authorizeRequests()
+                    .antMatchers("/health").permitAll()
+                    .antMatchers(GET, "/metrics").access(hasScope(NAKADI_READ_SCOPE))
+                    .antMatchers(GET, "/event-types").access(hasScope(NAKADI_READ_SCOPE))
+                    .antMatchers(GET, "/event-types/*").access(hasScope(NAKADI_READ_SCOPE))
+                    .antMatchers(POST, "/event-types").access(hasScope(EVENT_TYPE_WRITE_SCOPE))
+                    .antMatchers(PUT, "/event-types/*").access(hasScope(EVENT_TYPE_WRITE_SCOPE))
+                    .antMatchers(GET, "/event-types/*/events").access(hasScope(EVENT_STREAM_READ_SCOPE))
+                    .antMatchers(POST, "/event-types/*/events").access(hasScope(EVENT_STREAM_WRITE_SCOPE))
+                    .antMatchers(GET, "/event-types/*/partitions").access(hasScope(EVENT_STREAM_READ_SCOPE))
+                    .antMatchers(GET, "/event-types/*/partitions/*").access(hasScope(EVENT_STREAM_READ_SCOPE))
+                    .anyRequest().access(hasScope(UID));
+        }
+        else if (authMode == AuthMode.BASIC) {
+            http.authorizeRequests()
+                    .antMatchers("/health").permitAll()
+                    .anyRequest().access(hasScope(UID));
+        }
+        else {
+            http.authorizeRequests()
+                    .anyRequest().permitAll();
         }
     }
 
