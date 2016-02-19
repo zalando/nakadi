57bb0e3aaa6a6fb4475210171172b19ac438543f
Ricardo de Cillo <cillo.ricardo@gmail.com>
2016-02-19 17:18:37 +0100

    aruha-63 validate event schema on publish
    The purpose of this PR is to provide the simplest implementation
    possible to the validation of event schema. It's not focused on
    performance, which should be addressed in a future ticket.

--- src/main/java/de/zalando/aruha/nakadi/controller/EventPublishingController.java
+++ src/main/java/de/zalando/aruha/nakadi/controller/EventPublishingController.java
@@ -1,10 +1,18 @@
 package de.zalando.aruha.nakadi.controller;
 
 import com.codahale.metrics.annotation.Timed;
+import de.zalando.aruha.nakadi.domain.EventType;
+import de.zalando.aruha.nakadi.domain.ValidationStrategyConfiguration;
 import de.zalando.aruha.nakadi.exceptions.NakadiException;
 import de.zalando.aruha.nakadi.exceptions.NoSuchEventTypeException;
 import de.zalando.aruha.nakadi.repository.EventTypeRepository;
 import de.zalando.aruha.nakadi.repository.TopicRepository;
+import de.zalando.aruha.nakadi.validation.EventBodyMustRespectSchema;
+import de.zalando.aruha.nakadi.validation.EventValidator;
+import de.zalando.aruha.nakadi.validation.ValidationError;
+import de.zalando.aruha.nakadi.validation.ValidationStrategy;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -14,6 +22,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.NativeWebRequest;
+import org.zalando.problem.MoreStatus;
+import org.zalando.problem.Problem;
+
+import java.util.Optional;
 
 import static org.springframework.http.ResponseEntity.status;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
@@ -40,12 +52,19 @@ public class EventPublishingController {
         LOG.trace("Received event {} for event type {}", event, eventTypeName);
 
         try {
-            eventTypeRepository.findByName(eventTypeName);
+            EventType eventType = eventTypeRepository.findByName(eventTypeName);
 
-            // Will be replaced later:
-            final String partitionId = "1";
-            topicRepository.postEvent(eventTypeName, partitionId, event);
-            return status(HttpStatus.CREATED).build();
+            Optional<ValidationError> error = validateSchema(event, eventType);
+
+            if (error.isPresent()) {
+                Problem p = Problem.valueOf(MoreStatus.UNPROCESSABLE_ENTITY, error.get().getMessage());
+                return create(p, nativeWebRequest);
+            } else {
+                // Will be replaced later:
+                final String partitionId = "1";
+                topicRepository.postEvent(eventTypeName, partitionId, event);
+                return status(HttpStatus.CREATED).build();
+            }
         } catch (NoSuchEventTypeException e) {
             LOG.debug("Could not process event.", e);
             return create(e.asProblem(), nativeWebRequest);
@@ -55,4 +74,15 @@ public class EventPublishingController {
         }
     }
 
+    private Optional<ValidationError> validateSchema(@RequestBody String event, EventType eventType) {
+        try {
+            ValidationStrategy validationStrategy = new EventBodyMustRespectSchema();
+            ValidationStrategyConfiguration vsc = new ValidationStrategyConfiguration();
+            EventValidator validator = validationStrategy.materialize(eventType, vsc);
+            return validator.accepts(new JSONObject(event));
+        } catch (JSONException e) {
+            LOG.debug("Event parsing error.", e);
+            return Optional.of(new ValidationError("payload must be a valid json"));
+        }
+    }
 }
