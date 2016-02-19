adb3a8d75e3770a7286377f648e5619b7ab1ba98
v-stepanov <stepanov.vyacheslav@gmail.com>
2016-02-20 00:36:56 +0100

    ARUHA-56: working on oauth test

--- src/test/java/de/zalando/aruha/nakadi/security/EndpointsSecurityTest.java
+++ src/test/java/de/zalando/aruha/nakadi/security/EndpointsSecurityTest.java
@@ -2,11 +2,13 @@ package de.zalando.aruha.nakadi.security;
 
 
 import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Multimap;
 import de.zalando.aruha.nakadi.Application;
 import de.zalando.aruha.nakadi.config.SecuritySettings;
-import de.zalando.aruha.nakadi.utils.TestUtils;
+import de.zalando.aruha.nakadi.repository.TopicRepository;
+import de.zalando.aruha.nakadi.repository.db.EventTypeDbRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,17 +33,21 @@ import org.springframework.web.context.WebApplicationContext;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.Filter;
+import java.util.List;
 import java.util.Set;
 
+import static de.zalando.aruha.nakadi.utils.TestUtils.randomString;
 import static org.hamcrest.Matchers.isOneOf;
 import static org.hamcrest.Matchers.not;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.PUT;
 import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -77,12 +83,25 @@ public class EndpointsSecurityTest {
             when(settings.getAuthMode()).thenReturn(SecuritySettings.AuthMode.BASIC);
             return settings;
         }
+
+        @Bean
+        @Primary
+        public TopicRepository topicRepository() {
+            return mock(TopicRepository.class);
+        }
+
+        @Bean
+        @Primary
+        public EventTypeDbRepository dbRepository() {
+            return mock(EventTypeDbRepository.class);
+        }
     }
 
     public static final ResultMatcher STATUS_NOT_401_OR_403 = status()
             .is(not(isOneOf(UNAUTHORIZED.value(), FORBIDDEN.value())));
 
-    private static final String TOKEN_WITH_UID_SCOPE = TestUtils.randomString();
+    private static final String TOKEN_WITH_UID_SCOPE = randomString();
+    private static final String TOKEN_WITH_RANDOM_SCOPE = randomString();
 
     private static final Multimap<String, String> scopesForTokens = ArrayListMultimap.create();
 
@@ -109,9 +128,21 @@ public class EndpointsSecurityTest {
 
     private MockMvc mockMvc;
 
+    private static final List<Endpoint> endpoints = ImmutableList.of(
+            new Endpoint(GET, "/event-types"),
+            new Endpoint(POST, "/event-types"),
+            new Endpoint(GET, "/event-types/foo"),
+            new Endpoint(PUT, "/event-types/foo"),
+            new Endpoint(POST, "/event-types/foo/events"),
+            new Endpoint(GET, "/event-types/foo/events"),
+            new Endpoint(GET, "/event-types/foo/partitions"),
+            new Endpoint(GET, "/event-types/foo/partitions/bar"),
+            new Endpoint(GET, "/metrics"));
+
     @PostConstruct
     public void fillTokenMockData() {
         scopesForTokens.put(TOKEN_WITH_UID_SCOPE, uidScope);
+        scopesForTokens.put(TOKEN_WITH_RANDOM_SCOPE, randomString());
     }
 
     @Before
@@ -123,28 +154,33 @@ public class EndpointsSecurityTest {
     }
 
     @Test
-    public void checkGetEventTypes() throws Exception {
-        mockMvc
-                .perform(addTokenHeader(get("/event-types"), TOKEN_WITH_UID_SCOPE))
-                .andExpect(STATUS_NOT_401_OR_403);
-
-        mockMvc
-                .perform(get("/event-types"))
-                .andExpect(status().isUnauthorized());
+    public void basicAuthMode() throws Exception {
+        endpoints.forEach(this::checkUidEndpoint);
+        mockMvc.perform(get("/health")).andExpect(status().isOk());
     }
 
-    @Test
-    public void checkPostEventType() throws Exception {
-        mockMvc
-                .perform(addTokenHeader(post("/event-types"), TOKEN_WITH_UID_SCOPE))
-                .andExpect(STATUS_NOT_401_OR_403);
+    private void checkUidEndpoint(final Endpoint endpoint) {
+        try {
+            mockMvc.perform(addTokenHeader(endpoint.toRequestBuilder(), TOKEN_WITH_UID_SCOPE))
+                    .andExpect(STATUS_NOT_401_OR_403);
 
-        mockMvc
-                .perform(post("/event-types"))
-                .andExpect(status().isUnauthorized());
+            mockMvc.perform(addTokenHeader(endpoint.toRequestBuilder(), TOKEN_WITH_RANDOM_SCOPE))
+                    .andExpect(status().isForbidden());
+
+            mockMvc.perform(endpoint.toRequestBuilder())
+                    .andExpect(status().isUnauthorized());
+        }
+        catch (Exception e) {
+            throw new AssertionError("Error occurred when calling endpoint: " + endpoint, e);
+        }
+        catch (AssertionError e) {
+            throw new AssertionError("Assertion failed for endpoint: " + endpoint, e);
+        }
     }
 
-    private MockHttpServletRequestBuilder addTokenHeader(final MockHttpServletRequestBuilder builder, final String token) {
+    private MockHttpServletRequestBuilder addTokenHeader(final MockHttpServletRequestBuilder builder,
+                                                         final String token) {
         return builder.header("Authorization", "Bearer " + token);
     }
+
 }
