1b0fbc68d4e1d9cdd045d44d46519cff13813f07
Ricardo de Cillo <cillo.ricardo@gmail.com>
2016-02-19 10:02:34 +0100

    aruha-61 remove unused import

--- src/main/java/de/zalando/aruha/nakadi/domain/EventType.java
+++ src/main/java/de/zalando/aruha/nakadi/domain/EventType.java
@@ -1,7 +1,6 @@
 package de.zalando.aruha.nakadi.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.collect.Lists;
 
 import javax.validation.Valid;
