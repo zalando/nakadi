44a3bdbaaeec06ae516f79417433022d9c77a03d
Tobias Mühl <tobias.muehl@zalando.de>
2016-02-22 16:22:11 +0100

    ARUHA-71 Implement the strategy

--- src/main/java/de/zalando/aruha/nakadi/partitioning/OrderingKeyFieldsPartitioningStrategy.java
+++ src/main/java/de/zalando/aruha/nakadi/partitioning/OrderingKeyFieldsPartitioningStrategy.java
@@ -6,11 +6,18 @@ import org.json.JSONObject;
 import java.util.List;
 
 public class OrderingKeyFieldsPartitioningStrategy implements PartitioningStrategy {
+
     @Override
     public String calculatePartition(final EventType eventType, final JSONObject event, final int numberOfPartitions) {
         final List<String> orderingKeyFields = eventType.getOrderingKeyFields();
 
+        int hashValue = orderingKeyFields.stream()
+                // The problem is that JSONObject doesn't override hashCode(). Therefore convert it to a string first
+                // and then use hashCode()
+                .map(okf -> event.get(okf).toString().hashCode())
+                .mapToInt(hc -> hc)
+                .sum();
 
-        return "1";
+        return String.valueOf(hashValue % numberOfPartitions);
     }
 }
