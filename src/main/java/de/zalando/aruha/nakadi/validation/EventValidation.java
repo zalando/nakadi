f47a045c16eb986f47ff2a02f8bad15a26eeab7d
Ricardo de Cillo <cillo.ricardo@gmail.com>
2016-03-04 17:10:34 +0100

    aruha-108 remove unused prototyp validation code
    The EventValidation class was supposed to work a repository of event
    type validators but this code is not used in the project, since we have
    a guava chace in place.
    
    Guava chache provides more guarantees for generating event type
    validators one and one time only. The current code, even if thread safe,
    could lead to multiple event types been created because it's not
    synchronized.

--- src/main/java/de/zalando/aruha/nakadi/validation/EventValidation.java
+++ src/main/java/de/zalando/aruha/nakadi/validation/EventValidation.java
@@ -1,39 +1,10 @@
 package de.zalando.aruha.nakadi.validation;
 
-import com.google.common.base.Preconditions;
-import com.google.common.collect.Maps;
 import de.zalando.aruha.nakadi.domain.EventType;
 
-import java.util.Map;
-
 public class EventValidation {
-
-    private static final Map<String, EventTypeValidator> eventTypeValidators = Maps.newConcurrentMap();
-
     public static EventTypeValidator forType(final EventType eventType) {
-        return forType(eventType, false);
-    }
-
-    public static EventTypeValidator forType(final EventType eventType, final boolean incrementalEdit) {
-
-        final boolean contains = eventTypeValidators.containsKey(eventType.getName());
-        Preconditions.checkState(incrementalEdit || !contains, "Validator for EventType {} already defined",
-            eventType.getName());
-
-        if (!contains) {
-            final EventTypeValidator etsv = new EventTypeValidator(eventType);
-            eventTypeValidators.put(eventType.getName(), etsv);
-        }
-
-        return eventTypeValidators.get(eventType.getName());
-    }
-
-    public static EventTypeValidator lookup(final EventType eventType) {
-        return eventTypeValidators.get(eventType.getName());
-    }
-
-    public static void reset() {
-        eventTypeValidators.clear();
+        return new EventTypeValidator(eventType);
     }
 }
 
--- src/test/java/de/zalando/aruha/nakadi/validation/JSONSchemaValidationTest.java
+++ src/test/java/de/zalando/aruha/nakadi/validation/JSONSchemaValidationTest.java
@@ -5,15 +5,10 @@ import de.zalando.aruha.nakadi.domain.EventTypeSchema;
 import de.zalando.aruha.nakadi.domain.EventTypeSchema.Type;
 import de.zalando.aruha.nakadi.domain.ValidationStrategyConfiguration;
 import org.json.JSONObject;
-import org.junit.After;
 import org.junit.Test;
 
 import static de.zalando.aruha.nakadi.utils.IsOptional.isAbsent;
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertThat;
 
 public class JSONSchemaValidationTest {
@@ -23,39 +18,6 @@ public class JSONSchemaValidationTest {
     static {
         ValidationStrategy.register(EventBodyMustRespectSchema.NAME, new EventBodyMustRespectSchema());
         ValidationStrategy.register(FieldNameMustBeSet.NAME, new FieldNameMustBeSet());
-    }
-
-    @Test
-    public void eventValidationRegistryShouldResolveTypes() {
-        final EventType et = new EventType();
-        et.setName("some-event-type");
-
-        final EventType other = new EventType();
-        other.setName("some-other-event-type");
-
-        final EventTypeValidator validator = EventValidation.forType(et);
-        assertNotNull(validator);
-        assertEquals(validator, EventValidation.lookup(et));
-        assertNull(EventValidation.lookup(other));
-
-    }
-
-    @Test(expected = IllegalStateException.class)
-    public void eventValidationRegistryShouldDenyRespecification() {
-        final EventType et = new EventType();
-        et.setName("some-event-type");
-        EventValidation.forType(et);
-        EventValidation.forType(et);
-    }
-
-    @Test
-    public void eventValidationRegistryShouldAllowRespecificationIfRequestedExplicitly() {
-        final EventType et = new EventType();
-        et.setName("some-event-type");
-
-        final EventTypeValidator validator = EventValidation.forType(et);
-        assertNotNull(validator);
-        assertSame(validator, EventValidation.forType(et, true));
     }
 
     @Test
@@ -71,9 +33,8 @@ public class JSONSchemaValidationTest {
         final ValidationStrategyConfiguration vsc2 = new ValidationStrategyConfiguration();
         vsc2.setStrategyName(FieldNameMustBeSet.NAME);
         et.getValidationStrategies().add(vsc2);
-        EventValidation.forType(et).withConfiguration(vsc1).withConfiguration(vsc2);
 
-        final EventTypeValidator validator = EventValidation.lookup(et);
+        final EventTypeValidator validator = EventValidation.forType(et).withConfiguration(vsc1).withConfiguration(vsc2);
 
         final JSONObject validEvent = new JSONObject(
                 "{\"foo\": \"bar\", \"bar\": {\"foo\": \"baz\", \"bar\": \"baz\"}, \"extra\": \"i should be no problem\", \"name\": \"12345\"}");
@@ -115,9 +76,7 @@ public class JSONSchemaValidationTest {
                 "{\"overrides\": [{\"qualifier\": {\"field\": \"event-type\", \"match\" : \"D\"}, \"ignoredProperties\": [\"field-that-will-not-be-found\"]}]}"));
         et.getValidationStrategies().add(vsc1);
 
-        EventValidation.forType(et).withConfiguration(vsc1);
-
-        final EventTypeValidator validator = EventValidation.lookup(et);
+        final EventTypeValidator validator = EventValidation.forType(et).withConfiguration(vsc1);
 
         final JSONObject event = new JSONObject(
                 "{\"event-type\" : \"X\", \"extra\": \"i should be no problem\", \"name\": \"12345\", \"field-that-will-not-be-found\": {\"val\": \"i must be present since the matcher will not succeed\"}}");
@@ -142,9 +101,7 @@ public class JSONSchemaValidationTest {
                 "{\"overrides\": [{\"qualifier\": {\"field\": \"event-type\", \"match\" : \"D\"}, \"ignoredProperties\": [\"field-that-will-not-be-found\"]}]}"));
         et.getValidationStrategies().add(vsc1);
 
-        EventValidation.forType(et).withConfiguration(vsc1);
-
-        final EventTypeValidator validator = EventValidation.lookup(et);
+        final EventTypeValidator validator = EventValidation.forType(et).withConfiguration(vsc1);
 
         final JSONObject event = new JSONObject(
                 "{\"event-type\" : \"X\", \"field-that-will-not-be-found\": \"some useless value\", \"extra\": \"i should be no problem\", \"name\": \"12345\"}");
@@ -152,10 +109,5 @@ public class JSONSchemaValidationTest {
                 "{\"event-type\" : \"D\", \"extra\": \"i should be no problem\"}");
         assertThat(validator.validate(event), isAbsent());
         assertThat(validator.validate(eventDelete), isAbsent());
-    }
-
-    @After
-    public void teardown() {
-        EventValidation.reset();
     }
 }
