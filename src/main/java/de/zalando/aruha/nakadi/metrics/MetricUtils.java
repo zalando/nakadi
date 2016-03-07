55f1c11b292f519d0093aa49778c25b609f63b51
Tobias Mühl <tobias.muehl@zalando.de>
2016-03-07 16:42:51 +0100

    ARUHA-121 Measure the time needed to validate access tokens

--- src/main/java/de/zalando/aruha/nakadi/metrics/MetricUtils.java
+++ src/main/java/de/zalando/aruha/nakadi/metrics/MetricUtils.java
@@ -2,10 +2,11 @@ package de.zalando.aruha.nakadi.metrics;
 
 public class MetricUtils {
 
-    public static final String METRIC_PREFIX = "nakadi.eventtypes.";
+    public static final String NAKADI_PREFIX = "nakadi.";
+    public static final String EVENTTYPES_PREFIX = NAKADI_PREFIX + "eventtypes.";
 
     public static String metricNameFor(final String eventTypeName, final String metricName) {
-        return METRIC_PREFIX + eventTypeName.replace('.', '#') + "." + metricName;
+        return EVENTTYPES_PREFIX + eventTypeName.replace('.', '#') + "." + metricName;
     }
 
 }
