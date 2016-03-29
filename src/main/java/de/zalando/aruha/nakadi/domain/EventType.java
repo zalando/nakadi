7be6220c5947009911beba58596af8ec1f163e49
Ricardo de Cillo <cillo.ricardo@gmail.com>
2016-03-29 13:18:53 +0200

    aruha-131 make naming more specific
    Replace generalized names by more concrete ones in order to keep it
    simple.

--- src/main/java/de/zalando/aruha/nakadi/domain/EventType.java
+++ src/main/java/de/zalando/aruha/nakadi/domain/EventType.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.Pattern;
 import java.util.ArrayList;
 import java.util.List;
 
-import static de.zalando.aruha.nakadi.service.Registry.RANDOM_PARTITIONING_STRATEGY;
+import static de.zalando.aruha.nakadi.service.StrategiesRegistry.RANDOM_PARTITIONING_STRATEGY;
 import static java.util.Collections.unmodifiableList;
 
 public class EventType {
