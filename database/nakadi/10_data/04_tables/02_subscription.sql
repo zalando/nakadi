c5dee5aef115e11cc169eab024b0427e55eac9ea
Vyacheslav Stepanov <vyacheslav.stepanov@zalando.de>
2016-05-25 17:21:04 +0200

    ARUHA-202: added checks;

--- database/nakadi/10_data/04_tables/02_subscription.sql
+++ database/nakadi/10_data/04_tables/02_subscription.sql
@@ -2,9 +2,9 @@ CREATE TABLE IF NOT EXISTS zn_data.subscription (
   s_id varchar(36) NOT NULL PRIMARY KEY CHECK (s_id <> ''),
   s_subscription_object jsonb NOT NULL,
   CHECK (s_subscription_object->>'id' = s_id),
-  CHECK (s_subscription_object->>'owning_application' IS NOT NULL),
-  CHECK (s_subscription_object->>'event_types' IS NOT NULL),
-  CHECK (s_subscription_object->>'use_case' IS NOT NULL)
+  CHECK (s_subscription_object->>'owning_application' IS NOT NULL AND s_subscription_object->>'owning_application' <> ''),
+  CHECK (s_subscription_object->>'event_types' IS NOT NULL AND s_subscription_object->>'event_types' <> '[]'),
+  CHECK (s_subscription_object->>'use_case' IS NOT NULL AND s_subscription_object->>'use_case' <> '')
 );
 
 CREATE INDEX ON zn_data.subscription USING gin (s_subscription_object);
