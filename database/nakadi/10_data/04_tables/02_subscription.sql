92f5e6e8b4b5dd03238327adb4a1d9183fa543ed
Vyacheslav Stepanov <vyacheslav.stepanov@zalando.de>
2016-05-25 17:07:40 +0200

    ARUHA-202: Changed id field length; added composite index fields checks;

--- database/nakadi/10_data/04_tables/02_subscription.sql
+++ database/nakadi/10_data/04_tables/02_subscription.sql
@@ -1,7 +1,10 @@
 CREATE TABLE IF NOT EXISTS zn_data.subscription (
-  s_id varchar(255) NOT NULL PRIMARY KEY CHECK (s_id <> ''),
+  s_id varchar(36) NOT NULL PRIMARY KEY CHECK (s_id <> ''),
   s_subscription_object jsonb NOT NULL,
-  CHECK (s_subscription_object->>'id' = s_id)
+  CHECK (s_subscription_object->>'id' = s_id),
+  CHECK (s_subscription_object->>'owning_application' IS NOT NULL),
+  CHECK (s_subscription_object->>'event_types' IS NOT NULL),
+  CHECK (s_subscription_object->>'use_case' IS NOT NULL)
 );
 
 CREATE INDEX ON zn_data.subscription USING gin (s_subscription_object);
