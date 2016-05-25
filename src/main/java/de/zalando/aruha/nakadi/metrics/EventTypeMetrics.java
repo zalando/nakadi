b535c9e054be3db062a95cbbfe2ae664e45edd23
antban <dmitry.sorokin@gmail.com>
2016-05-25 14:58:34 +0200

    ARUHA-201 Add meter for events per second publishing

--- src/main/java/de/zalando/aruha/nakadi/metrics/EventTypeMetrics.java
+++ src/main/java/de/zalando/aruha/nakadi/metrics/EventTypeMetrics.java
@@ -2,6 +2,7 @@ package de.zalando.aruha.nakadi.metrics;
 
 import com.codahale.metrics.Counter;
 import com.codahale.metrics.Histogram;
+import com.codahale.metrics.Meter;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.Timer;
 import com.google.common.annotations.VisibleForTesting;
@@ -18,12 +19,14 @@ public class EventTypeMetrics {
 
     private final Histogram eventsPerBatchHistogram;
     private final Timer publishingTimer;
+    private final Meter eventCountMeter;
     private final Histogram averageEventSizeInBytesHistogram;
     private final ConcurrentMap<Integer, Counter> statusCodeCounter = new ConcurrentHashMap<>();
 
     public EventTypeMetrics(final String eventTypeName, final MetricRegistry metricRegistry) {
         this.eventTypeName = eventTypeName;
         this.metricRegistry = metricRegistry;
+        eventCountMeter = metricRegistry.meter(metricNameFor(eventTypeName, "publishing.events"));
         eventsPerBatchHistogram = metricRegistry.histogram(metricNameFor(eventTypeName, "publishing.eventsPerBatch"));
         averageEventSizeInBytesHistogram = metricRegistry.histogram(metricNameFor(eventTypeName, "publishing.averageEventSizeInBytes"));
         publishingTimer = metricRegistry.timer(metricNameFor(eventTypeName, "publishing"));
@@ -31,6 +34,7 @@ public class EventTypeMetrics {
 
     public void reportSizing(int eventsPerBatch, int totalEventSize) {
         eventsPerBatchHistogram.update(eventsPerBatch);
+        eventCountMeter.mark(eventsPerBatch);
         averageEventSizeInBytesHistogram.update(eventsPerBatch == 0 ? 0 : totalEventSize / eventsPerBatch);
     }
 
