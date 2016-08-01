5c879c8560745200edbd1f3b675ec85e0d9f362e
Andrey Dyachkov <andrey.dyachkov@zalando.de>
2016-08-01 14:58:36 +0200

    ARUHA-224: changed scope check

--- src/main/java/org/zalando/nakadi/security/ScopeHelper.java
+++ src/main/java/org/zalando/nakadi/security/ScopeHelper.java
@@ -4,31 +4,22 @@ import org.zalando.nakadi.exceptions.IllegalScopeException;
 
 import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class ScopeHelper {
 
     public static void checkScopes(final Set<String> allowedScopes, final Set<String> clientScopes) {
-        if (isNotEmptyScope(allowedScopes)){
-            if (hasNoAllowedScopes(allowedScopes, clientScopes)) {
-                throw new IllegalScopeException(getMissingScopes(allowedScopes, clientScopes));
-            }
+        if (isEmptyScope(allowedScopes) ) {
+            return;
         }
+
+        allowedScopes.stream()
+                .filter(clientScopes::contains)
+                .findAny()
+                .orElseThrow(() -> new IllegalScopeException(allowedScopes));
     }
 
-    private static boolean isNotEmptyScope(final Set<String> allowedScopes) {
-        return Objects.nonNull(allowedScopes) && !allowedScopes.isEmpty();
-    }
-
-    private static boolean hasNoAllowedScopes(final Set<String> allowedScopes, final Set<String> clientScopes) {
-        return clientScopes.stream()
-                .noneMatch(allowedScopes::contains);
-    }
-
-    private static Set<String> getMissingScopes(final Set<String> allowedScopes, final Set<String> clientScopes) {
-        return allowedScopes.stream()
-                .filter(scope -> !clientScopes.contains(scope))
-                .collect(Collectors.toSet());
+    private static boolean isEmptyScope(final Set<String> allowedScopes) {
+        return Objects.isNull(allowedScopes) || allowedScopes.isEmpty();
     }
 
 }
