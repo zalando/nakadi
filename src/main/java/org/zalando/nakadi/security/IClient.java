ef27967741630afc9a67bf2463c17c502f88c980
Andrey Dyachkov <andrey.dyachkov@zalando.de>
2016-08-02 08:53:45 +0200

    ARUHA-224: fixed checkstyle

--- src/main/java/org/zalando/nakadi/security/IClient.java
+++ src/main/java/org/zalando/nakadi/security/IClient.java
@@ -4,14 +4,14 @@ import java.util.Set;
 
 public interface IClient {
 
-    public static final IClient FULL_ACCESS = new IClient() {
+    IClient FULL_ACCESS = new IClient() {
         @Override
         public boolean authenticate(String clientId) {return true;}
         @Override
         public void authorize(Set<String> allowedScopes) {}
     };
 
-    public boolean authenticate(final String clientId);
+    boolean authenticate(final String clientId);
 
-    public void authorize(final Set<String> allowedScopes);
+    void authorize(final Set<String> allowedScopes);
 }
