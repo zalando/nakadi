b347a6c5d4e8fb506a7705f38af446d3d5ca2228
Vyacheslav Stepanov <vyacheslav.stepanov@zalando.de>
2016-08-04 19:24:58 +0200

    ARUHA-311: added security for new endpoint;

--- src/main/java/org/zalando/nakadi/config/SecurityConfiguration.java
+++ src/main/java/org/zalando/nakadi/config/SecurityConfiguration.java
@@ -60,6 +60,7 @@ public class SecurityConfiguration extends ResourceServerConfigurerAdapter {
                     .antMatchers(POST, "/event-types/**").access(hasScope(eventTypeWriteScope))
                     .antMatchers(PUT, "/event-types/**").access(hasScope(eventTypeWriteScope))
                     .antMatchers(POST, "/subscriptions/**").access(hasScope(eventStreamReadScope))
+                    .antMatchers(GET, "/subscriptions/**").access(hasScope(eventStreamReadScope))
                     .antMatchers(GET, "/health/**").permitAll()
                     .anyRequest().access(hasScope(uidScope));
         }
