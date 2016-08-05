803c04ba132ced297168c38252469a1152ea2679
Viktor Buldakov <viktor.buldakov@zalando.de>
2016-08-05 16:02:26 +0200

    ARUHA-123: change build

--- src/main/java/org/zalando/nakadi/config/NakadiConfig.java
+++ src/main/java/org/zalando/nakadi/config/NakadiConfig.java
@@ -8,21 +8,24 @@ import com.codahale.metrics.jvm.ThreadStatesGaugeSet;
 import com.codahale.metrics.servlets.MetricsServlet;
 import com.ryantenney.metrics.spring.config.annotation.EnableMetrics;
 import com.ryantenney.metrics.spring.config.annotation.MetricsConfigurerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.zalando.nakadi.plugin.api.ApplicationService;
 import org.zalando.nakadi.plugin.api.ApplicationServiceFactory;
 import org.zalando.nakadi.plugin.api.SystemProperties;
 import org.zalando.nakadi.repository.zookeeper.ZooKeeperHolder;
 import org.zalando.nakadi.repository.zookeeper.ZooKeeperLockFactory;
 import org.zalando.nakadi.service.subscription.zk.ZkSubscriptionClientFactory;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
-import org.springframework.core.task.TaskExecutor;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.lang.management.ManagementFactory;
 
@@ -30,6 +33,8 @@ import java.lang.management.ManagementFactory;
 @EnableMetrics
 @EnableScheduling
 public class NakadiConfig {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(NakadiConfig.class);
 
     @Bean
     public TaskExecutor taskExecutor() {
@@ -81,11 +86,11 @@ public class NakadiConfig {
     @Bean
     @SuppressWarnings("unchecked")
     public ApplicationService applicationService(@Value("${nakadi.auth.plugin.factory}") final String factoryName,
-                                                 final SystemProperties systemProperties)
-    {
+                                                 final SystemProperties systemProperties,
+                                                 final DefaultResourceLoader loader) {
         try {
-            final Class<ApplicationServiceFactory> factoryClass = (Class<ApplicationServiceFactory>) NakadiConfig.class.getClassLoader()
-                    .loadClass(factoryName);
+            LOGGER.info("Initialize application service factory: " + factoryName);
+            final Class<ApplicationServiceFactory> factoryClass = (Class<ApplicationServiceFactory>) loader.getClassLoader().loadClass(factoryName);
             final ApplicationServiceFactory factory = factoryClass.newInstance();
             return factory.init(systemProperties);
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
