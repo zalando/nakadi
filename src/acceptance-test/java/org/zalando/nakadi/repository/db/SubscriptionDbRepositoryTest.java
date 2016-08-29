9907d99d3e11aa2b47c83f90768e8488b50aa2c8
Vyacheslav Stepanov <vyacheslav.stepanov@zalando.de>
2016-08-29 17:27:27 +0200

    ARUHA-335: changed repository method to allow pagination and filtering by event types; adjusted tests;

--- src/acceptance-test/java/org/zalando/nakadi/repository/db/SubscriptionDbRepositoryTest.java
+++ src/acceptance-test/java/org/zalando/nakadi/repository/db/SubscriptionDbRepositoryTest.java
@@ -16,7 +16,9 @@ import org.zalando.nakadi.util.UUIDGenerator;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
+import static java.util.Collections.emptySet;
 import static java.util.stream.Collectors.toList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.isEmptyOrNullString;
@@ -108,26 +110,30 @@ public class SubscriptionDbRepositoryTest extends AbstractDbRepositoryTest {
         testSubscriptions.sort(SUBSCRIPTION_CREATION_DATE_DESC_COMPARATOR);
         testSubscriptions.forEach(this::insertSubscriptionToDB);
 
-        final List<Subscription> subscriptions = repository.listSubscriptions();
+        final List<Subscription> subscriptions = repository.listSubscriptions(emptySet(), Optional.empty(), 0, 10);
         assertThat(subscriptions, equalTo(testSubscriptions));
     }
 
     @Test
-    public void whenListSubscriptionsByOwningApplicationThenOk() throws ServiceUnavailableException {
+    public void whenListSubscriptionsByOwningApplicationAndEventTypeThenOk() throws ServiceUnavailableException {
 
         final List<Subscription> testSubscriptions = ImmutableList.of(
-                randomSubscription().withOwningApplication("myapp1").build(),
-                randomSubscription().withOwningApplication("myapp1").build(),
-                randomSubscription().withOwningApplication("myapp2").build());
-
+                randomSubscription().withOwningApplication("app").withEventType("et1").build(),
+                randomSubscription().withOwningApplication("app").withEventTypes(ImmutableSet.of("et2", "et1")).build(),
+                randomSubscription().withOwningApplication("app").withEventType("et1").build(),
+                randomSubscription().withOwningApplication("app").withEventType("et2").build(),
+                randomSubscription().withOwningApplication("app").withEventTypes(ImmutableSet.of("et2", "et3")).build(),
+                randomSubscription().withOwningApplication("app2").withEventType("et1").build(),
+                randomSubscription().withOwningApplication("app2").withEventType("et2").build());
         testSubscriptions.forEach(this::insertSubscriptionToDB);
 
         final List<Subscription> expectedSubscriptions = testSubscriptions.stream()
-                .filter(sub -> "myapp1".equals(sub.getOwningApplication()))
+                .filter(sub -> "app".equals(sub.getOwningApplication()) && sub.getEventTypes().contains("et1"))
                 .sorted(SUBSCRIPTION_CREATION_DATE_DESC_COMPARATOR)
                 .collect(toList());
 
-        final List<Subscription> subscriptions = repository.listSubscriptionsForOwningApplication("myapp1");
+        final List<Subscription> subscriptions = repository.listSubscriptions(ImmutableSet.of("et1"),
+                Optional.of("app"), 0, 10);
         assertThat(subscriptions, equalTo(expectedSubscriptions));
     }
 
