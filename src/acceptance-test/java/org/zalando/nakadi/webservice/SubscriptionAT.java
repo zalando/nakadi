02130fb5cd043408f45701beb6e09231bc5e29c2
Viktor Buldakov <viktor.buldakov@zalando.de>
2016-09-07 16:43:46 +0200

    ARUHA-336: fix tests

--- src/acceptance-test/java/org/zalando/nakadi/webservice/SubscriptionAT.java
+++ src/acceptance-test/java/org/zalando/nakadi/webservice/SubscriptionAT.java
@@ -135,7 +135,7 @@ public class SubscriptionAT extends BaseAT {
                 "\",\"cursor_token\":\"abc\"}]";
         commitCursors(subscription, cursor)
                 .then()
-                .statusCode(HttpStatus.SC_OK);
+                .statusCode(HttpStatus.SC_CREATED);
 
         // check that offset is actually committed to Zookeeper
         final CuratorFramework curator = ZookeeperTestUtils.createCurator(ZOOKEEPER_URL);
