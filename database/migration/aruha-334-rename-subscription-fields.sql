fae9b2836bcb5944d56cb441a207b15f6e4f6a64
Viktor Buldakov <viktor.buldakov@zalando.de>
2016-09-16 14:13:29 +0200

    fix script to supprot postgresql 9.4

--- database/migration/aruha-334-rename-subscription-fields.sql
+++ database/migration/aruha-334-rename-subscription-fields.sql
@@ -1,6 +1,6 @@
 UPDATE zn_data.subscription
-SET s_subscription_object = jsonb_set(s_subscription_object, '{consumer_group}', '"default"')
-WHERE s_subscription_object->>'consumer_group' = 'none';
+SET s_subscription_object = replace(s_subscription_object::text, '"none"', '"default"')::jsonb
+WHERE s_subscription_object::text LIKE '%"none"%';
 
 UPDATE zn_data.subscription
 SET s_subscription_object = replace(s_subscription_object::text, 'start_from', 'read_from')::jsonb
