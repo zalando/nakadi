2355c6b294450fc3d2b90353660e258d00a55126
Viktor Buldakov <viktor.buldakov@zalando.de>
2016-10-20 16:32:22 +0200

    ARUHA-356: fix cc

--- src/test/java/org/zalando/nakadi/service/ThrottleServiceTest.java
+++ src/test/java/org/zalando/nakadi/service/ThrottleServiceTest.java
@@ -23,52 +23,52 @@ public class ThrottleServiceTest {
 
     @Test
     public void throttleByBytes() throws InterruptedException {
-        ThrottlingService service = new ThrottlingService(1000, 1, zkConfigurationService);
-        ThrottleResult result = service.mark("test1", "test1", 100, 1);
+        final ThrottlingService service = new ThrottlingService(1000, 1, zkConfigurationService);
+        final ThrottleResult result = service.mark("test1", "test1", 100, 1);
         Assert.assertTrue(result.isThrottled());
 
-        ThrottleResult result2 = service.mark("test1", "test1", 10, 1);
+        final ThrottleResult result2 = service.mark("test1", "test1", 10, 1);
         Assert.assertFalse(result2.isThrottled());
 
         Thread.sleep(2000);
 
-        ThrottleResult result3 = service.mark("test1", "test1", 10, 1);
+        final ThrottleResult result3 = service.mark("test1", "test1", 10, 1);
         Assert.assertFalse(result3.isThrottled());
     }
 
     @Test
     public void throttleByMessages() throws InterruptedException {
-        ThrottlingService service = new ThrottlingService(1000, 1, zkConfigurationService);
-        ThrottleResult result = service.mark("test2", "test2", 1, 100);
+        final ThrottlingService service = new ThrottlingService(1000, 1, zkConfigurationService);
+        final ThrottleResult result = service.mark("test2", "test2", 1, 100);
         Assert.assertTrue(result.isThrottled());
 
-        ThrottleResult result2 = service.mark("test2", "test2", 1, 10);
+        final ThrottleResult result2 = service.mark("test2", "test2", 1, 10);
         Assert.assertFalse(result2.isThrottled());
 
         Thread.sleep(2000);
 
-        ThrottleResult result3 = service.mark("test2", "test2", 1, 10);
+        final ThrottleResult result3 = service.mark("test2", "test2", 1, 10);
         Assert.assertFalse(result3.isThrottled());
     }
 
     @Test
     public void throttleByBatches() throws InterruptedException {
-        ThrottlingService service = new ThrottlingService(1000, 1, zkConfigurationService);
-        ThrottleResult result = service.mark("test3", "test3", 1, 1);
+        final ThrottlingService service = new ThrottlingService(1000, 1, zkConfigurationService);
+        final ThrottleResult result = service.mark("test3", "test3", 1, 1);
         Assert.assertFalse(result.isThrottled());
 
-        ThrottleResult result1 = service.mark("test3", "test3", 1, 1);
+        final ThrottleResult result1 = service.mark("test3", "test3", 1, 1);
         Assert.assertFalse(result1.isThrottled());
 
-        ThrottleResult result2 = service.mark("test3", "test3", 1, 1);
+        final ThrottleResult result2 = service.mark("test3", "test3", 1, 1);
         Assert.assertFalse(result2.isThrottled());
 
-        ThrottleResult result3 = service.mark("test3", "test3", 1, 1);
+        final ThrottleResult result3 = service.mark("test3", "test3", 1, 1);
         Assert.assertTrue(result3.isThrottled());
 
         Thread.sleep(2000);
 
-        ThrottleResult result4 = service.mark("test3", "test3", 1, 1);
+        final ThrottleResult result4 = service.mark("test3", "test3", 1, 1);
         Assert.assertFalse(result4.isThrottled());
 
     }
