bde6da79a0f30be42269c904260610167a5a050b
Ricardo de Cillo <cillo.ricardo@gmail.com>
2016-11-11 11:50:38 +0100

    aruha-400 fix typo

--- src/main/java/org/zalando/nakadi/validation/schema/DeprecatedSchemaChangeConstraint.java
+++ src/main/java/org/zalando/nakadi/validation/schema/DeprecatedSchemaChangeConstraint.java
@@ -6,7 +6,7 @@ import org.zalando.nakadi.domain.EventType;
 import java.util.Optional;
 
 public class DeprecatedSchemaChangeConstraint implements SchemaEvolutionConstraint {
-    private final String errorMessage = "schema from an event types with deprecated compatibility mode cannot not be " +
+    private final String errorMessage = "schema from an event types with deprecated compatibility mode cannot be " +
             "changed. Please, upgrade this event type to compatible mode in order to be able to change the schema.";
     @Override
     public Optional<SchemaEvolutionIncompatibility> validate(final EventType original, final EventType eventType) {
