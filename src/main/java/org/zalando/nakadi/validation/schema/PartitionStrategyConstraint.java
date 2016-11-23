fae5ea36dcd5e796cac51fa9fbc4dbf380dfd7dd
Ricardo de Cillo <cillo.ricardo@gmail.com>
2016-11-23 14:46:48 +0100

    aruha-400 fix partition constraints checking

--- src/main/java/org/zalando/nakadi/validation/schema/PartitionStrategyConstraint.java
+++ src/main/java/org/zalando/nakadi/validation/schema/PartitionStrategyConstraint.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 public class PartitionStrategyConstraint implements SchemaEvolutionConstraint {
     @Override
     public Optional<SchemaEvolutionIncompatibility> validate(final EventType original, final EventTypeBase eventType) {
-        if (eventType.getPartitionStrategy().equals(original.getPartitionStrategy())) {
+        if (!eventType.getPartitionStrategy().equals(original.getPartitionStrategy())) {
             return Optional.of(new SchemaEvolutionIncompatibility("changing partition_strategy is not allowed"));
         } else {
             return Optional.empty();
