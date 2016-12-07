1c43dfad2f632482262e761a636f72a0eac547e4
Andrey Dyachkov <andrey.dyachkov@zalando.de>
2016-12-07 11:32:30 +0100

    aruha-401: set schema after event type creation

--- src/acceptance-test/java/org/zalando/nakadi/repository/db/SchemaRepositoryTest.java
+++ src/acceptance-test/java/org/zalando/nakadi/repository/db/SchemaRepositoryTest.java
@@ -32,9 +32,9 @@ public class SchemaRepositoryTest extends AbstractDbRepositoryTest {
         final EventTypeSchemaBase schemaBase = new EventTypeSchemaBase(EventTypeSchemaBase.Type.JSON_SCHEMA, "schema");
         final EventType eventType = EventTypeTestBuilder.builder()
                 .name("test_et_name_schemarepositorytest")
-                .schema(new EventTypeSchema(schemaBase, "1.0.2", DateTime.parse("2016-12-07T10:44:16.378+01:00")))
                 .build();
         insertEventType(eventType);
+        eventType.setSchema(new EventTypeSchema(schemaBase, "1.0.2", DateTime.parse("2016-12-07T10:44:16.378+01:00")));
         insertSchema(eventType);
         eventType.setSchema(new EventTypeSchema(schemaBase, "2.10.3", DateTime.parse("2016-12-07T10:54:16.778+01:00")));
         insertSchema(eventType);
