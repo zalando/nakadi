d61e29db78b89cd1bf880a8294fa567b1b753442
Ricardo de Cillo <cillo.ricardo@gmail.com>
2016-12-08 13:31:33 +0100

    aruha-404 fix unrelated acceptance test
    Fixes #500

--- src/acceptance-test/java/org/zalando/nakadi/webservice/BlockEventPublishingAT.java
+++ src/acceptance-test/java/org/zalando/nakadi/webservice/BlockEventPublishingAT.java
@@ -1,11 +1,16 @@
 package org.zalando.nakadi.webservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.response.Response;
 import org.apache.http.HttpStatus;
+import org.echocat.jomon.runtime.concurrent.RetryForSpecifiedTimeStrategy;
+import org.json.JSONObject;
 import org.junit.Test;
+import org.zalando.nakadi.config.JsonConfig;
 import org.zalando.nakadi.domain.EventType;
 import org.zalando.nakadi.service.FloodService;
+import org.zalando.nakadi.utils.JsonTestHelper;
 import org.zalando.nakadi.webservice.utils.NakadiTestUtils;
 import org.zalando.problem.MoreStatus;
 
@@ -13,8 +18,13 @@ import java.io.IOException;
 import java.text.MessageFormat;
 
 import static com.jayway.restassured.RestAssured.given;
+import static org.echocat.jomon.runtime.concurrent.Retryer.executeWithRetry;
 
 public class BlockEventPublishingAT extends BaseAT {
+
+    private static final String FLOODERS_URL = "/settings/flooders";
+    private static final ObjectMapper MAPPER = (new JsonConfig()).jacksonObjectMapper();
+    private static final JsonTestHelper JSON_HELPER = new JsonTestHelper(MAPPER);
 
     @Test
     public void whenPublishingToBlockedEventTypeThen429() throws IOException {
@@ -27,15 +37,43 @@ public class BlockEventPublishingAT extends BaseAT {
         final FloodService.Flooder flooder =
                 new FloodService.Flooder(eventType.getName(), FloodService.Type.PRODUCER_ET);
         SettingsControllerAT.blockFlooder(flooder);
+
+        waitForBlock(eventType);
+
         publishEvent(eventType)
                 .then()
                 .statusCode(MoreStatus.TOO_MANY_REQUESTS.getStatusCode())
                 .header("Retry-After", "300");
 
         SettingsControllerAT.unblockFlooder(flooder);
+
+        waitForUnblock(eventType);
+
         publishEvent(eventType)
                 .then()
                 .statusCode(HttpStatus.SC_OK);
+    }
+
+    private void waitForUnblock(final EventType eventType) {
+        executeWithRetry(() -> {
+            return !getFlooders().getJSONObject("producers").getJSONArray("event_types").toList()
+                    .contains(eventType.getName());
+        }, new RetryForSpecifiedTimeStrategy<Boolean>(5000).withResultsThatForceRetry(false).withWaitBetweenEachTry(
+                500));
+    }
+
+    private void waitForBlock(final EventType eventType) {
+        executeWithRetry(() -> {
+            return getFlooders().getJSONObject("producers").getJSONArray("event_types").toList()
+                            .contains(eventType.getName());
+        }, new RetryForSpecifiedTimeStrategy<Boolean>(5000).withResultsThatForceRetry(false).withWaitBetweenEachTry(
+                        500));
+    }
+
+    private JSONObject getFlooders() {
+        return new JSONObject(given()
+                                .header("accept", "application/json").get(FLOODERS_URL)
+                                .getBody().asString());
     }
 
     private Response publishEvent(final EventType eventType) {
