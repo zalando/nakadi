30da84aaa864d95ed8618ec1d6d9aa9ccadbf4e9
Lionel Montrieux <lionel.montrieux@zalando.de>
2016-12-30 14:00:33 +0100

    ARUHA-473 BatchFactory method to process batches as Strings

--- src/main/java/org/zalando/nakadi/domain/BatchFactory.java
+++ src/main/java/org/zalando/nakadi/domain/BatchFactory.java
@@ -16,4 +16,33 @@ public class BatchFactory {
         return batch;
     }
 
+    public static List<BatchItem> from(final String events) {
+        final List<BatchItem> batch = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        int brackets = 0;
+        for (int i = 1; i < events.length() - 1; i++) {
+            if (events.charAt(i) == '{') {
+                brackets++;
+            }
+            if (events.charAt(i) == '}') {
+                brackets--;
+            }
+            if (!((brackets == 0) && (events.charAt(i) == ','))) {
+                sb.append(events.charAt(i));
+            }
+            if (brackets == 0) {
+                if (sb.length() > 0) {
+                    batch.add(new BatchItem(sb.toString()));
+                }
+                sb = new StringBuilder();
+            }
+        }
+
+        if (sb.length() != 0) {
+            batch.add(new BatchItem(sb.toString()));
+        }
+
+        return batch;
+    }
+
 }
