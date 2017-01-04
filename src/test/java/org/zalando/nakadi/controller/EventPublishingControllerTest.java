20aed22fb4e9c9599e4006fdbe7ffc6d25c596b8
Lionel Montrieux <lionel.montrieux@zalando.de>
2017-01-04 17:59:58 +0100

    ARUHA-473 Don't parse input to JSON in EventPublishingController

--- src/test/java/org/zalando/nakadi/controller/EventPublishingControllerTest.java
+++ src/test/java/org/zalando/nakadi/controller/EventPublishingControllerTest.java
@@ -2,6 +2,7 @@ package org.zalando.nakadi.controller;
 
 import com.codahale.metrics.MetricRegistry;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONException;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -14,6 +15,8 @@ import org.zalando.nakadi.config.JsonConfig;
 import org.zalando.nakadi.config.SecuritySettings;
 import org.zalando.nakadi.domain.BatchItemResponse;
 import org.zalando.nakadi.domain.EventPublishResult;
+import org.zalando.nakadi.domain.EventPublishingStatus;
+import org.zalando.nakadi.domain.EventPublishingStep;
 import org.zalando.nakadi.exceptions.InternalNakadiException;
 import org.zalando.nakadi.exceptions.NoSuchEventTypeException;
 import org.zalando.nakadi.metrics.EventTypeMetricRegistry;
@@ -41,6 +44,7 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standal
 import static org.zalando.nakadi.domain.EventPublishingStatus.ABORTED;
 import static org.zalando.nakadi.domain.EventPublishingStatus.FAILED;
 import static org.zalando.nakadi.domain.EventPublishingStatus.SUBMITTED;
+import static org.zalando.nakadi.domain.EventPublishingStep.NONE;
 import static org.zalando.nakadi.domain.EventPublishingStep.PARTITIONING;
 import static org.zalando.nakadi.domain.EventPublishingStep.PUBLISHING;
 import static org.zalando.nakadi.domain.EventPublishingStep.VALIDATING;
@@ -85,7 +89,7 @@ public class EventPublishingControllerTest {
 
     @Test
     public void whenResultIsSubmittedThen200() throws Exception {
-        final EventPublishResult result = new EventPublishResult(SUBMITTED, null, null);
+        final EventPublishResult result = new EventPublishResult(SUBMITTED, null, submittedResponses(1));
 
         Mockito
                 .doReturn(result)
@@ -99,11 +103,12 @@ public class EventPublishingControllerTest {
 
     @Test
     public void whenInvalidPostBodyThen400() throws Exception {
-        final String expectedPayload = "{\"type\":\"http://httpstatus.es/400\"," +
-                "\"title\":\"Bad Request\",\"status\":400," +
-                "\"detail\":\"A JSONArray text must start with '[' at 1 [character 2 line 1]\"}";
-        postBatch(TOPIC, "invalid json array").andExpect(status().isBadRequest())
-                .andExpect(content().string(expectedPayload));
+
+        Mockito.doThrow(new JSONException("Error"))
+                .when(publisher)
+                .publish(any(String.class), eq(TOPIC), any(Client.class));
+
+        postBatch(TOPIC, "invalid json array").andExpect(status().isBadRequest());
     }
 
     @Test
@@ -148,7 +153,7 @@ public class EventPublishingControllerTest {
 
     @Test
     public void publishedEventsAreReportedPerEventType() throws Exception {
-        final EventPublishResult success = new EventPublishResult(SUBMITTED, null, null);
+        final EventPublishResult success = new EventPublishResult(SUBMITTED, null, submittedResponses(3));
         Mockito
                 .doReturn(success)
                 .doReturn(success)
@@ -174,6 +179,26 @@ public class EventPublishingControllerTest {
         final List<BatchItemResponse> responses = new ArrayList<>();
         responses.add(response);
 
+        return responses;
+    }
+
+    private List<BatchItemResponse> submittedResponses(final int number) {
+        return responses(number, SUBMITTED, PUBLISHING);
+    }
+
+    private List<BatchItemResponse> abortedResponses(final int number) {
+        return responses(number, ABORTED, NONE);
+    }
+
+    private List<BatchItemResponse> responses(final int number, final EventPublishingStatus status,
+                                              final EventPublishingStep step) {
+        final List<BatchItemResponse> responses = new ArrayList<>();
+        for (int i = 0; i < number; i++) {
+            final BatchItemResponse response = new BatchItemResponse();
+            response.setPublishingStatus(status);
+            response.setStep(step);
+            responses.add(response);
+        }
         return responses;
     }
 
