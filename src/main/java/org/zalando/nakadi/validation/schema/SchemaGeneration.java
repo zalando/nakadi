cedb99c6a6c25810edf48ea28cfca2bc0a850fa5
Ricardo de Cillo <cillo.ricardo@gmail.com>
2017-01-16 15:50:37 +0100

    Improved generation of schemas
    This commit implements a more sofisticated merging function which
    generates more concise schemas.

--- src/main/java/org/zalando/nakadi/validation/schema/SchemaGeneration.java
+++ src/main/java/org/zalando/nakadi/validation/schema/SchemaGeneration.java
@@ -10,18 +10,49 @@ public class SchemaGeneration {
     public JSONObject mergeSchema(final JSONObject thisSchema, final JSONObject thatSchema) {
         if (thisSchema.length() == 0) {
             return thatSchema;
-        // TODO: ugly hack to generate anyOf schemas. Could be optimized to generate smaller schemas.
         } else if (JSONCompare.compareJSON(thisSchema, thatSchema, JSONCompareMode.STRICT).failed()) {
-            return new JSONObject().put("anyOf", new JSONArray(Lists.newArrayList(thisSchema, thatSchema)));
+            if (thisSchema.getString("type").equals(thatSchema.getString("type"))
+                    && thisSchema.getString("type").equals("object")) {
+                mergeProperties(thisSchema, thatSchema);
+            } else {
+                return new JSONObject().put("anyOf", new JSONArray(Lists.newArrayList(thisSchema, thatSchema)));
+            }
+            return thisSchema;
         } else {
             return thisSchema;
+        }
+    }
+
+    private void mergeProperties(final JSONObject thisSchema, final JSONObject thatSchema) {
+        if (thisSchema.has("properties") || thatSchema.has("properties")) {
+            if (!thisSchema.has("properties")) {
+                thisSchema.put("properties", new JSONObject());
+            } else if (!thatSchema.has("properties")){
+                thatSchema.put("properties", new JSONObject());
+            }
+
+            for (final String key : thisSchema.getJSONObject("properties").keySet()) {
+                if (thatSchema.getJSONObject("properties").has(key)) {
+                    final JSONObject mergedSchema = mergeSchema(thisSchema.getJSONObject("properties")
+                                    .getJSONObject(key),
+                                    thatSchema.getJSONObject("properties").getJSONObject(key));
+                    thisSchema.getJSONObject("properties").put(key, mergedSchema);
+                }
+            }
+
+            for (final String key : thatSchema.getJSONObject("properties").keySet()) {
+                if (!thisSchema.getJSONObject("properties").has(key)) {
+                    thisSchema.getJSONObject("properties").put(key,
+                            thatSchema.getJSONObject("properties").getJSONObject(key));
+                }
+            }
         }
     }
 
     public JSONObject schemaFor(final Object event) {
         if (event instanceof String) {
             return new JSONObject().put("type", "string");
-        } else if (event instanceof Integer) {
+        } else if (event instanceof Integer || event instanceof Double) {
             return new JSONObject().put("type", "number");
         } else if (event instanceof Boolean) {
             return new JSONObject().put("type", "boolean");
@@ -56,7 +87,8 @@ public class SchemaGeneration {
         if (event.length() > 0) {
             JSONObject itemSchema = new JSONObject();
             for (int i = 0; i < event.length(); i = i + 1) {
-                itemSchema = mergeSchema(itemSchema, schemaFor(event.get(i)));
+                final JSONObject thatSchema = schemaFor(event.get(i));
+                itemSchema = mergeSchema(itemSchema, thatSchema);
             }
             schema.put("items", itemSchema);
         }
