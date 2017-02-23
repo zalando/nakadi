8716b80bc98cf3179b5862734d18880624fce5b9
Andrey Dyachkov <andrey.dyachkov@zalando.de>
2017-02-23 20:02:23 +0100

    aruha-527: added processing for storage already exists exception

--- src/main/java/org/zalando/nakadi/repository/tool/DefaultStorage.java
+++ src/main/java/org/zalando/nakadi/repository/tool/DefaultStorage.java
@@ -1,18 +1,21 @@
 package org.zalando.nakadi.repository.tool;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.zalando.nakadi.domain.Storage;
+import org.zalando.nakadi.exceptions.DuplicatedStorageIdException;
+import org.zalando.nakadi.exceptions.InternalNakadiException;
 import org.zalando.nakadi.repository.db.StorageDbRepository;
-
-import java.util.Optional;
 
 @Service
 public class DefaultStorage implements ApplicationRunner {
 
+    private static final Logger LOG = LoggerFactory.getLogger(DefaultStorage.class);
     private static final String DEFAULT_STORAGE = "default";
     private final Environment environment;
     private final StorageDbRepository storageDbRepository;
@@ -25,18 +28,19 @@ public class DefaultStorage implements ApplicationRunner {
     }
 
     @Override
-    public void run(final ApplicationArguments args) throws Exception {
-        final Optional<Storage> storageOpt = storageDbRepository.getStorage(DEFAULT_STORAGE);
-        if (!storageOpt.isPresent()) {
-            final Storage storage = new Storage();
-            storage.setId(DEFAULT_STORAGE);
-            storage.setType(Storage.Type.KAFKA);
-            storage.setConfiguration(new Storage.KafkaConfiguration(
-                    environment.getProperty("nakadi.zookeeper.exhibitor.brokers"),
-                    Integer.valueOf(environment.getProperty("nakadi.zookeeper.exhibitor.port", "0")),
-                    environment.getProperty("nakadi.zookeeper.brokers"),
-                    environment.getProperty("nakadi.zookeeper.kafkaNamespace", "")));
+    public void run(final ApplicationArguments args) throws InternalNakadiException {
+        final Storage storage = new Storage();
+        storage.setId(DEFAULT_STORAGE);
+        storage.setType(Storage.Type.KAFKA);
+        storage.setConfiguration(new Storage.KafkaConfiguration(
+                environment.getProperty("nakadi.zookeeper.exhibitor.brokers"),
+                Integer.valueOf(environment.getProperty("nakadi.zookeeper.exhibitor.port", "0")),
+                environment.getProperty("nakadi.zookeeper.brokers"),
+                environment.getProperty("nakadi.zookeeper.kafkaNamespace", "")));
+        try {
             storageDbRepository.createStorage(storage);
+        } catch (final DuplicatedStorageIdException e) {
+            LOG.info("Creation of default storage failed: {}", e.getMessage());
         }
     }
 }
