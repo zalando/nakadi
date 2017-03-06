ec2073eabbcb9e27f0eaf11e8d4747c2a7e15ce5
dehora <bill@dehora.net>
2017-03-06 14:54:24 +0000

    ARUHA-641 Write batch events directly to outputstream.
    If SEND_BATCH_VIA_OUTPUT_STREAM is enabled, send the batch and its events
    directly to the consumer's OutputStream as bytes instead of building up an
    intermediate batch String via createStreamEvent. The standard parts of the
    batch are allocated statically as bytes instead of per call.
    
    This is done to reduce the number of allocations generated for a batch and is
    based on some performance tuning investigations.

--- src/main/java/org/zalando/nakadi/service/EventStream.java
+++ src/main/java/org/zalando/nakadi/service/EventStream.java
@@ -36,6 +36,22 @@ public class EventStream {
     public static final String BATCH_SEPARATOR = "\n";
     public static final Charset UTF8 = Charset.forName("UTF-8");
 
+    private static final byte B_BATCH_SEPARATOR = '\n';
+    private static final byte[] B_CURSOR_PARTITION_BEGIN = "{\"cursor\":{\"partition\":\"".getBytes(StandardCharsets.UTF_8);
+    private static final byte[] B_OFFSET_BEGIN = "\",\"offset\":\"".getBytes(StandardCharsets.UTF_8);
+    private static final byte[] B_CURSOR_PARTITION_END = "\"}".getBytes(StandardCharsets.UTF_8);
+    private static final byte[] B_CLOSE_CURLY_BRACKET = "}".getBytes(StandardCharsets.UTF_8);
+    private static final byte[] B_EVENTS_ARRAY_BEGIN = ",\"events\":[".getBytes(StandardCharsets.UTF_8);
+    private static final byte B_COMMA_DELIM = ',';
+    private static final byte B_CLOSE_BRACKET = ']';
+    private static final int B_FIXED_BYTE_COUNT = B_CURSOR_PARTITION_BEGIN.length
+        + B_OFFSET_BEGIN.length
+        + B_CURSOR_PARTITION_END.length
+        + B_EVENTS_ARRAY_BEGIN.length
+        + 1 // B_COMMA_DELIM
+        + 1 // B_CLOSE_BRACKET
+        + B_CLOSE_CURLY_BRACKET.length
+        + 1; //B_BATCH_SEPARATOR
 
     private final OutputStream outputStream;
     private final EventConsumer eventConsumer;
@@ -176,13 +192,52 @@ public class EventStream {
 
     private void sendBatch(final NakadiCursor topicPosition, final List<String> currentBatch)
             throws IOException {
-        // create stream event batch for current partition and send it; if there were
-        // no events, it will be just a keep-alive
-        final String streamEvent = createStreamEvent(cursorConverter.convert(topicPosition), currentBatch);
-        final byte[] batchBytes = streamEvent.getBytes(UTF8);
-        outputStream.write(batchBytes);
-        bytesFlushedMeter.mark(batchBytes.length);
-        outputStream.flush();
+        if(featureToggleService.isFeatureEnabled(FeatureToggleService.Feature.SEND_BATCH_VIA_OUTPUT_STREAM)) {
+            writeStreamEvent(outputStream, cursorConverter.convert(topicPosition), currentBatch);
+        } else {
+            // create stream event batch for current partition and send it; if there were
+            // no events, it will be just a keep-alive
+            final String streamEvent = createStreamEvent(cursorConverter.convert(topicPosition), currentBatch);
+            final byte[] batchBytes = streamEvent.getBytes(UTF8);
+            outputStream.write(batchBytes);
+            bytesFlushedMeter.mark(batchBytes.length);
+            outputStream.flush();
+        }
+    }
+
+    private void writeStreamEvent(final OutputStream os, final Cursor cursor, final List<String> events)
+        throws IOException {
+
+        int byteCount = B_FIXED_BYTE_COUNT;
+
+        os.write(B_CURSOR_PARTITION_BEGIN);
+        final byte[] partition = cursor.getPartition().getBytes(StandardCharsets.UTF_8);
+        os.write(partition);
+        byteCount += partition.length;
+        os.write(B_OFFSET_BEGIN);
+        final byte[] offset = cursor.getOffset().getBytes(StandardCharsets.UTF_8);
+        os.write(offset);
+        byteCount += offset.length;
+        os.write(B_CURSOR_PARTITION_END);
+        if (!events.isEmpty()) {
+            os.write(B_EVENTS_ARRAY_BEGIN);
+            for (int i = 0; i < events.size(); i++) {
+                final byte[] event = events.get(i).getBytes(StandardCharsets.UTF_8);
+                os.write(event);
+                byteCount += event.length;
+                if(i < (events.size() - 1)) {
+                    os.write(B_COMMA_DELIM);
+                }
+                else {
+                    os.write(B_CLOSE_BRACKET);
+                }
+            }
+        }
+        os.write(B_CLOSE_CURLY_BRACKET);
+        os.write(B_BATCH_SEPARATOR);
+
+        bytesFlushedMeter.mark(byteCount);
+        os.flush();
     }
 
     public void close() throws IOException {
