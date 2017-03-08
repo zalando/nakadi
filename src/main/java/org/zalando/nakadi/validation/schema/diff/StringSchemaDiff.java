b26c2eae06849394bff9a8cd8754e6db431a0b1e
Ricardo de Cillo <cillo.ricardo@gmail.com>
2017-03-08 13:36:11 +0100

    issue-596 get string representatino for comparison

--- src/main/java/org/zalando/nakadi/validation/schema/diff/StringSchemaDiff.java
+++ src/main/java/org/zalando/nakadi/validation/schema/diff/StringSchemaDiff.java
@@ -17,7 +17,7 @@ class StringSchemaDiff {
             addChange("maxLength", ATTRIBUTE_VALUE_CHANGED, jsonPath, changes);
         } else if (!Objects.equals(stringSchemaOriginal.getMinLength(), stringSchemaUpdate.getMinLength())) {
             addChange("minLength", ATTRIBUTE_VALUE_CHANGED, jsonPath, changes);
-        } else if (!stringSchemaOriginal.getPattern().equals(stringSchemaUpdate.getPattern())) {
+        } else if (!stringSchemaOriginal.getPattern().pattern().equals(stringSchemaUpdate.getPattern().pattern())) {
             addChange("pattern", ATTRIBUTE_VALUE_CHANGED, jsonPath, changes);
         }
     }
