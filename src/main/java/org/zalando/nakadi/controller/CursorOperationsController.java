078ce2ba6b482007e274d21d9c1b4bba46694693
Ricardo de Cillo <cillo.ricardo@gmail.com>
2017-03-29 16:02:02 +0200

    aruha-531 properly handle exceptions

--- src/main/java/org/zalando/nakadi/controller/CursorOperationsController.java
+++ src/main/java/org/zalando/nakadi/controller/CursorOperationsController.java
@@ -1,8 +1,11 @@
 package org.zalando.nakadi.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +19,7 @@ import org.zalando.nakadi.domain.NakadiCursorLag;
 import org.zalando.nakadi.domain.ShiftedNakadiCursor;
 import org.zalando.nakadi.exceptions.InvalidCursorException;
 import org.zalando.nakadi.exceptions.NakadiException;
+import org.zalando.nakadi.exceptions.runtime.InvalidCursorOperation;
 import org.zalando.nakadi.exceptions.runtime.MyNakadiRuntimeException1;
 import org.zalando.nakadi.problem.ValidationProblem;
 import org.zalando.nakadi.service.CursorConverter;
@@ -25,6 +29,8 @@ import org.zalando.nakadi.view.CursorDistanceQuery;
 import org.zalando.nakadi.view.CursorDistanceResult;
 import org.zalando.nakadi.view.CursorLag;
 import org.zalando.nakadi.view.ShiftedCursor;
+import org.zalando.problem.MoreStatus;
+import org.zalando.problem.Problem;
 import org.zalando.problem.spring.web.advice.Responses;
 
 import javax.validation.Valid;
@@ -37,6 +43,8 @@ import static org.springframework.http.ResponseEntity.status;
 
 @RestController
 public class CursorOperationsController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CursorOperationsController.class);
 
     private final CursorConverter cursorConverter;
     private final CursorOperationsService cursorOperationsService;
@@ -113,6 +121,31 @@ public class CursorOperationsController {
                 .collect(Collectors.toList());
 
         return status(OK).body(viewResult);
+    }
+
+    @ExceptionHandler(InvalidCursorOperation.class)
+    public ResponseEntity<Problem> invalidCursorOperation(final InvalidCursorOperation e,
+                                                          final NativeWebRequest request) {
+        return Responses.create(Problem.valueOf(MoreStatus.UNPROCESSABLE_ENTITY,
+                clientErrorMessage(e.getReason())), request);
+    }
+
+    private String clientErrorMessage(final InvalidCursorOperation.Reason reason) {
+        switch (reason) {
+            case INVERTED_TIMELINE_ORDER: return "Inverted timelines. Final cursor must correspond to a newer timeline " +
+                    "than initial cursor.";
+            case TIMELINE_NOT_FOUND: return "Timeline not found. It might happen in case the cursor refers to a " +
+                    "timeline that has already expired.";
+            case INVERTED_OFFSET_ORDER: return "Inverted offsets. Final cursor offsets must be newer than initial " +
+                    "cursor offsets";
+            case PARTITION_NOT_FOUND: return "Partition not found.";
+            case CURSORS_WITH_DIFFERENT_PARTITION: return "Cursors with different partition. Pairs of cursors should " +
+                    "have matching partitions.";
+            default: {
+                LOG.error("Unexpected invalid cursor operation reason " + reason);
+                throw new MyNakadiRuntimeException1();
+            }
+        }
     }
 
     private CursorLag toCursorLag(final NakadiCursorLag nakadiCursorLag) {
