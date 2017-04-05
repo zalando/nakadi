33d334bae91c75f52b9a315bba355a52e0d65acb
antban <dmitry.sorokin@gmail.com>
2017-04-05 11:50:41 +0200

    ARUHA-530 Remove unnecessary setters

--- src/main/java/org/zalando/nakadi/view/TimelineView.java
+++ src/main/java/org/zalando/nakadi/view/TimelineView.java
@@ -66,40 +66,4 @@ public class TimelineView {
     public Timeline.StoragePosition getLatestPosition() {
         return latestPosition;
     }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public void setEventType(String eventType) {
-        this.eventType = eventType;
-    }
-
-    public void setOrder(int order) {
-        this.order = order;
-    }
-
-    public void setStorageId(String storageId) {
-        this.storageId = storageId;
-    }
-
-    public void setTopic(String topic) {
-        this.topic = topic;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public void setSwitchedAt(Date switchedAt) {
-        this.switchedAt = switchedAt;
-    }
-
-    public void setCleanedUpAt(Date cleanedUpAt) {
-        this.cleanedUpAt = cleanedUpAt;
-    }
-
-    public void setLatestPosition(Timeline.StoragePosition latestPosition) {
-        this.latestPosition = latestPosition;
-    }
 }
