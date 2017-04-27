bcfd2cb9662de80976d085605b991eba752c106c
Vyacheslav Stepanov <vyacheslav.stepanov@zalando.de>
2017-04-27 15:39:21 +0200

    ARUHA-771: Added missing alter

--- database/migration/aruha-771-subscription-hash.sql
+++ database/migration/aruha-771-subscription-hash.sql
@@ -3,4 +3,8 @@ SET ROLE zalando_nakadi_data_owner;
 DROP INDEX subscription_s_subscription_object_idx;
 DROP INDEX subscription_expr_expr1_expr2_idx;
 
+ALTER TABLE zn_data.subscription ADD COLUMN s_key_fields_hash varchar(100) NOT NULL DEFAULT '';
+
+-- between these two alters we need to run script that fills "s_key_fields_hash" field for all existing subscriptions
+
 ALTER TABLE zn_data.subscription ADD CONSTRAINT subscription_key_fields_hash_is_unique UNIQUE (s_key_fields_hash);
