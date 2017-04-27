2c06f5c9bfb80a6541dceb797c945bd7cd06afbd
Vyacheslav Stepanov <vyacheslav.stepanov@zalando.de>
2017-04-27 15:40:43 +0200

    ARUHA-771: added migration helper

--- database/migration/aruha-771-subscription-hash.sql
+++ database/migration/aruha-771-subscription-hash.sql
@@ -5,6 +5,6 @@ DROP INDEX subscription_expr_expr1_expr2_idx;
 
 ALTER TABLE zn_data.subscription ADD COLUMN s_key_fields_hash varchar(100) NOT NULL DEFAULT '';
 
--- between these two alters we need to run script that fills "s_key_fields_hash" field for all existing subscriptions
+-- between these two alters we need to run Aruha771MigrationHelper script that fills "s_key_fields_hash" field for all existing subscriptions
 
 ALTER TABLE zn_data.subscription ADD CONSTRAINT subscription_key_fields_hash_is_unique UNIQUE (s_key_fields_hash);
