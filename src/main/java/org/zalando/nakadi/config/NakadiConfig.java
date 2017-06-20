ac69aa039d0013268917c5548ed5164264960b7e
Lionel Montrieux <lionel.montrieux@zalando.de>
2017-06-20 15:45:45 +0200

    ARUHA-759 Configure authorization plugin
    Load the authorization plugin according to the application configuration

--- src/main/java/org/zalando/nakadi/config/NakadiConfig.java
+++ src/main/java/org/zalando/nakadi/config/NakadiConfig.java
@@ -14,11 +14,13 @@ import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.zalando.nakadi.domain.Storage;
-import org.zalando.nakadi.exceptions.runtime.DuplicatedStorageException;
 import org.zalando.nakadi.exceptions.InternalNakadiException;
+import org.zalando.nakadi.exceptions.runtime.DuplicatedStorageException;
 import org.zalando.nakadi.plugin.api.ApplicationService;
 import org.zalando.nakadi.plugin.api.ApplicationServiceFactory;
 import org.zalando.nakadi.plugin.api.SystemProperties;
+import org.zalando.nakadi.plugin.api.authz.AuthorizationService;
+import org.zalando.nakadi.plugin.api.authz.AuthorizationServiceFactory;
 import org.zalando.nakadi.repository.db.StorageDbRepository;
 import org.zalando.nakadi.repository.zookeeper.ZooKeeperHolder;
 import org.zalando.nakadi.repository.zookeeper.ZooKeeperLockFactory;
@@ -57,6 +59,21 @@ public class NakadiConfig {
             return factory.init(systemProperties);
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
             throw new BeanCreationException("Can't create ApplicationService " + factoryName, e);
+        }
+    }
+
+    @Bean
+    public AuthorizationService authorizationService(@Value("${nakadi.authz.plugin.factory}") final String factoryName,
+                                                     final SystemProperties systemProperties,
+                                                     final DefaultResourceLoader loader) {
+        try {
+            LOGGER.info("Initialize per-resource authorization service factory: " + factoryName);
+            final Class<AuthorizationServiceFactory> factoryClass =
+                    (Class<AuthorizationServiceFactory>) loader.getClassLoader().loadClass(factoryName);
+            final AuthorizationServiceFactory factory = factoryClass.newInstance();
+            return factory.init(systemProperties);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            throw new BeanCreationException("Can't create AuthorizationService " + factoryName, e);
         }
     }
 
