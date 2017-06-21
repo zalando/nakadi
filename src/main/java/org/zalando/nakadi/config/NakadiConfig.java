382b8980b65d828d94216d7dad2d6e8c7fbea223
Lionel Montrieux <lionel.montrieux@zalando.de>
2017-06-21 11:54:28 +0200

    ARUHA-759 Move plugins configuration
    Move plugins configuration to their own file

--- src/main/java/org/zalando/nakadi/config/NakadiConfig.java
+++ src/main/java/org/zalando/nakadi/config/NakadiConfig.java
@@ -2,25 +2,18 @@ package org.zalando.nakadi.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.zalando.nakadi.domain.Storage;
 import org.zalando.nakadi.exceptions.InternalNakadiException;
 import org.zalando.nakadi.exceptions.runtime.DuplicatedStorageException;
-import org.zalando.nakadi.plugin.api.ApplicationService;
-import org.zalando.nakadi.plugin.api.ApplicationServiceFactory;
 import org.zalando.nakadi.plugin.api.SystemProperties;
-import org.zalando.nakadi.plugin.api.authz.AuthorizationService;
-import org.zalando.nakadi.plugin.api.authz.AuthorizationServiceFactory;
 import org.zalando.nakadi.repository.db.StorageDbRepository;
 import org.zalando.nakadi.repository.zookeeper.ZooKeeperHolder;
 import org.zalando.nakadi.repository.zookeeper.ZooKeeperLockFactory;
@@ -44,37 +37,6 @@ public class NakadiConfig {
     @Bean
     public SystemProperties systemProperties(final ApplicationContext context) {
         return name -> context.getEnvironment().getProperty(name);
-    }
-
-    @Bean
-    @SuppressWarnings("unchecked")
-    public ApplicationService applicationService(@Value("${nakadi.auth.plugin.factory}") final String factoryName,
-                                                 final SystemProperties systemProperties,
-                                                 final DefaultResourceLoader loader) {
-        try {
-            LOGGER.info("Initialize application service factory: " + factoryName);
-            final Class<ApplicationServiceFactory> factoryClass =
-                    (Class<ApplicationServiceFactory>) loader.getClassLoader().loadClass(factoryName);
-            final ApplicationServiceFactory factory = factoryClass.newInstance();
-            return factory.init(systemProperties);
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
-            throw new BeanCreationException("Can't create ApplicationService " + factoryName, e);
-        }
-    }
-
-    @Bean
-    public AuthorizationService authorizationService(@Value("${nakadi.authz.plugin.factory}") final String factoryName,
-                                                     final SystemProperties systemProperties,
-                                                     final DefaultResourceLoader loader) {
-        try {
-            LOGGER.info("Initialize per-resource authorization service factory: " + factoryName);
-            final Class<AuthorizationServiceFactory> factoryClass =
-                    (Class<AuthorizationServiceFactory>) loader.getClassLoader().loadClass(factoryName);
-            final AuthorizationServiceFactory factory = factoryClass.newInstance();
-            return factory.init(systemProperties);
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
-            throw new BeanCreationException("Can't create AuthorizationService " + factoryName, e);
-        }
     }
 
     @Bean
