37fdf4fd9fba1bd87f478217c84b6d11b8e98088
Lionel Montrieux <lionel.montrieux@zalando.de>
2017-08-02 10:26:18 +0200

    ARUHA-953 Remove unused import

--- src/test/java/org/zalando/nakadi/controller/EventStreamControllerTest.java
+++ src/test/java/org/zalando/nakadi/controller/EventStreamControllerTest.java
@@ -58,7 +58,6 @@ import java.net.UnknownHostException;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
