d33b3130ea3b62afdba90eee40262a03b99f17d4
Lionel Montrieux <lionel.montrieux@zalando.de>
2017-08-21 14:54:05 +0200

    ARUHA-1075 Rename field for consistency

--- database/nakadi/10_data/04_tables/06_authorization.sql
+++ database/nakadi/10_data/04_tables/06_authorization.sql
@@ -1,9 +1,9 @@
-CREATE TYPE az_role AS ENUM('admin', 'readers', 'writers');
+CREATE TYPE az_operation AS ENUM('admin', 'readers', 'writers');
 
 CREATE TABLE zn_data.authorization (
   az_id              UUID                     NOT NULL PRIMARY KEY,
   az_resource        VARCHAR(255)             NOT NULL,
-  az_list            az_role                  NOT NULL,
+  az_operation       az_operation             NOT NULL,
   az_data_type       VARCHAR(255)             NOT NULL,
   az_value           VARCHAR(255)             NOT NULL
 );
