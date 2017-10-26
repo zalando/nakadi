f4244cb84d8afad912861b02a670e9408edb9470
Andrey Dyachkov <andrey.dyachkov@zalando.de>
2017-10-26 15:50:22 +0200

    aruha-1247: bounded jetty queue

--- src/main/java/org/zalando/nakadi/config/JettyConfig.java
+++ src/main/java/org/zalando/nakadi/config/JettyConfig.java
@@ -8,22 +8,27 @@ import org.springframework.boot.context.embedded.jetty.JettyServerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.concurrent.ArrayBlockingQueue;
+
 @Configuration
 public class JettyConfig {
     @Bean
     public JettyEmbeddedServletContainerFactory jettyEmbeddedServletContainerFactory(
             @Value("${server.port:8080}") final String port,
             @Value("${jetty.threadPool.maxThreads:200}") final String maxThreads,
-            @Value("${jetty.threadPool.minThreads:8}") final String minThreads,
-            @Value("${jetty.threadPool.idleTimeout:60000}") final String idleTimeout) {
+            @Value("${jetty.threadPool.minThreads:10}") final String minThreads,
+            @Value("${jetty.threadPool.idleTimeout:60000}") final String idleTimeout,
+            @Value("${jetty.threadPool.queueSize:3000}") final String queueSize) {
         final JettyEmbeddedServletContainerFactory factory =
                 new JettyEmbeddedServletContainerFactory(Integer.valueOf(port));
-        factory.addServerCustomizers((JettyServerCustomizer) server -> {
-            final QueuedThreadPool threadPool = server.getBean(QueuedThreadPool.class);
-            threadPool.setMaxThreads(Integer.valueOf(maxThreads));
-            threadPool.setMinThreads(Integer.valueOf(minThreads));
-            threadPool.setIdleTimeout(Integer.valueOf(idleTimeout));
 
+        final QueuedThreadPool queuedThreadPool = new QueuedThreadPool(Integer.valueOf(maxThreads),
+                Integer.valueOf(minThreads),
+                Integer.valueOf(idleTimeout),
+                new ArrayBlockingQueue<>(Integer.valueOf(queueSize)));
+        factory.setThreadPool(queuedThreadPool);
+
+        factory.addServerCustomizers((JettyServerCustomizer) server -> {
             final GzipHandler gzipHandler = new GzipHandler();
             gzipHandler.setHandler(server.getHandler());
             gzipHandler.setSyncFlush(true);
