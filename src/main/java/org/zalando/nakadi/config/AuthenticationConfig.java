5129a08720a2f7bb35d2162e190cfbddd9c4045e
Andrey Dyachkov <andrey.dyachkov@zalando.de>
2017-11-13 15:48:28 +0100

    aruha-1322: set max per route open connections to 20

--- src/main/java/org/zalando/nakadi/config/AuthenticationConfig.java
+++ src/main/java/org/zalando/nakadi/config/AuthenticationConfig.java
@@ -2,13 +2,22 @@ package org.zalando.nakadi.config;
 
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.Timer;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.web.client.RestTemplate;
 import org.zalando.nakadi.metrics.MetricUtils;
+import org.zalando.stups.oauth2.spring.authorization.DefaultUserRolesProvider;
+import org.zalando.stups.oauth2.spring.server.DefaultAuthenticationExtractor;
 import org.zalando.stups.oauth2.spring.server.TokenInfoResourceServerTokenServices;
 
 @Configuration
@@ -17,17 +26,24 @@ public class AuthenticationConfig {
 
     @Bean
     public ResourceServerTokenServices zalandoResourceTokenServices(final SecuritySettings settings,
-                                                                    final MetricRegistry metricRegistry) {
+                                                                    final MetricRegistry metricRegistry,
+                                                                    final RestTemplate restTemplate) {
         return new MeasuringTokenInfoResourceServerTokenServices(
-                settings.getTokenInfoUrl(), settings.getClientId(), metricRegistry);
+                settings.getTokenInfoUrl(), settings.getClientId(), metricRegistry, restTemplate);
     }
 
     public static class MeasuringTokenInfoResourceServerTokenServices extends TokenInfoResourceServerTokenServices {
         private final Timer timer;
 
-        public MeasuringTokenInfoResourceServerTokenServices(final String tokenInfoEndpointUrl, final String clientId,
-                                                             final MetricRegistry metricRegistry) {
-            super(tokenInfoEndpointUrl, clientId);
+        public MeasuringTokenInfoResourceServerTokenServices(final String tokenInfoEndpointUrl,
+                                                             final String clientId,
+                                                             final MetricRegistry metricRegistry,
+                                                             final RestTemplate restTemplate) {
+            super(tokenInfoEndpointUrl,
+                    clientId,
+                    new DefaultAuthenticationExtractor(),
+                    new DefaultUserRolesProvider(),
+                    restTemplate);
             timer = metricRegistry.timer(MetricUtils.NAKADI_PREFIX + "general.accessTokenValidation");
         }
 
@@ -40,5 +56,41 @@ public class AuthenticationConfig {
                 context.stop();
             }
         }
+    }
+
+    @Bean
+    public PoolingHttpClientConnectionManager poolingHttpClientConnectionManager() {
+        final PoolingHttpClientConnectionManager result = new PoolingHttpClientConnectionManager();
+        result.setMaxTotal(20);
+        result.setDefaultMaxPerRoute(20);
+        return result;
+    }
+
+    @Bean
+    public RequestConfig requestConfig() {
+        final RequestConfig result = RequestConfig.custom()
+                .setConnectionRequestTimeout(500)
+                .setConnectTimeout(1000)
+                .setSocketTimeout(2000)
+                .build();
+        return result;
+    }
+
+    @Bean
+    public CloseableHttpClient httpClient(final PoolingHttpClientConnectionManager poolingHttpClientConnectionManager,
+                                          final RequestConfig requestConfig) {
+        final CloseableHttpClient result = HttpClientBuilder
+                .create()
+                .setConnectionManager(poolingHttpClientConnectionManager)
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+        return result;
+    }
+
+    @Bean
+    public RestTemplate restTemplate(final HttpClient httpClient) {
+        final HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+        return new RestTemplate(requestFactory);
     }
 }
