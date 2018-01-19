3103c2304b7c751434e98dbb4d4c08be3950740b
Lionel Montrieux <lionel.montrieux@zalando.de>
2018-01-19 14:47:30 +0100

    ARUHA-1462 Use curator recipe for instant feature toggle
    Also delete low value test

--- src/main/java/org/zalando/nakadi/util/FeatureToggleServiceZk.java
+++ src/main/java/org/zalando/nakadi/util/FeatureToggleServiceZk.java
@@ -1,16 +1,14 @@
 package org.zalando.nakadi.util;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
 import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.cache.TreeCache;
 import org.apache.zookeeper.KeeperException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.zalando.nakadi.repository.zookeeper.ZooKeeperHolder;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 public class FeatureToggleServiceZk implements FeatureToggleService {
 
@@ -18,34 +16,34 @@ public class FeatureToggleServiceZk implements FeatureToggleService {
     private static final String PREFIX = "/nakadi/feature_toggle/";
 
     private final ZooKeeperHolder zkHolder;
-    private final LoadingCache<Feature, Boolean> cache;
+    private TreeCache featuresCache;
 
     public FeatureToggleServiceZk(final ZooKeeperHolder zkHolder) {
         this.zkHolder = zkHolder;
+    }
 
-        cache = CacheBuilder.newBuilder()
-                .maximumSize(100)
-                .expireAfterWrite(30, TimeUnit.SECONDS)
-                .build(new CacheLoader<Feature, Boolean>() {
-                    @Override
-                    public Boolean load(final Feature key) throws Exception {
-                        return isFeatureEnabledInZk(key);
-                    }
-                });
+    @PostConstruct
+    public void init() {
+        try {
+            this.featuresCache = TreeCache.newBuilder(zkHolder.get(), PREFIX).setCacheData(false).build();
+            this.featuresCache.start();
+        } catch (final Exception e) {
+            LOG.error(e.getMessage(), e);
+        }
+    }
 
+    @PreDestroy
+    public void cleanUp() {
+        this.featuresCache.close();
     }
 
     public boolean isFeatureEnabled(final Feature feature) {
         try {
-            return cache.get(feature);
-        } catch (final ExecutionException e) {
+            return featuresCache.getCurrentData(PREFIX + feature.getId()) != null;
+        } catch (final Exception e) {
             LOG.warn("Error occurred when checking if feature '" + feature.getId() + "' is toggled", e);
             return false;
         }
-    }
-
-    private Boolean isFeatureEnabledInZk(final Feature feature) throws Exception {
-        return null != zkHolder.get().checkExists().forPath(PREFIX + feature.getId());
     }
 
     public void setFeature(final FeatureWrapper feature) {
