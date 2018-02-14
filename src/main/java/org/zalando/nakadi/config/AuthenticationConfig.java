8a28556cba665601e9243f1b5b508fe164d80c55
Andrey Dyachkov <andrey.dyachkov@zalando.de>
2018-02-14 16:19:14 +0100

    aruha-602: return correct problem json response

--- src/main/java/org/zalando/nakadi/config/AuthenticationConfig.java
+++ src/main/java/org/zalando/nakadi/config/AuthenticationConfig.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 import org.springframework.web.client.RestTemplate;
@@ -116,6 +117,10 @@ public class AuthenticationConfig {
                 } else {
                     return localService.loadAuthentication(accessToken);
                 }
+            } catch (OAuth2Exception e) {
+                throw e;
+            } catch (Exception e) {
+                throw new OAuth2Exception(e.getMessage(), e);
             } finally {
                 context.stop();
             }
--- src/test/java/org/zalando/nakadi/security/RealmModeAuthenticationTest.java
+++ src/test/java/org/zalando/nakadi/security/RealmModeAuthenticationTest.java
@@ -3,6 +3,7 @@ package org.zalando.nakadi.security;
 import org.junit.Test;
 import org.zalando.nakadi.config.SecuritySettings;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class RealmModeAuthenticationTest extends AuthenticationTest {
@@ -24,19 +25,27 @@ public class RealmModeAuthenticationTest extends AuthenticationTest {
 
             // token with wrong realm
             mockMvc.perform(endpoint.withToken(TOKEN_WITH_WRONG_REALM).toRequestBuilder())
-                    .andExpect(status().isForbidden());
+                    .andExpect(status().isForbidden())
+                    .andExpect(content().string("{\"type\":\"https://httpstatus.es/403\",\"title\":\"Forbidden\"," +
+                            "\"status\":403,\"detail\":\"Insufficient realms for this resource\"}"));
 
             // token with random scope
             mockMvc.perform(endpoint.withToken(TOKEN_WITH_RANDOM_SCOPE).toRequestBuilder())
-                    .andExpect(status().isForbidden());
+                    .andExpect(status().isForbidden())
+                    .andExpect(content().string("{\"type\":\"https://httpstatus.es/403\",\"title\":\"Forbidden\"," +
+                            "\"status\":403,\"detail\":\"Insufficient realms for this resource\"}"));
 
             // token with uid scope
             mockMvc.perform(endpoint.withToken(TOKEN_WITH_UID_SCOPE).toRequestBuilder())
-                    .andExpect(status().isForbidden());
+                    .andExpect(status().isForbidden())
+                    .andExpect(content().string("{\"type\":\"https://httpstatus.es/403\",\"title\":\"Forbidden\"," +
+                            "\"status\":403,\"detail\":\"Insufficient realms for this resource\"}"));
 
             // no token at all
             mockMvc.perform(endpoint.withToken(null).toRequestBuilder())
-                    .andExpect(status().isUnauthorized());
+                    .andExpect(status().isUnauthorized())
+                    .andExpect(content().string("{\"type\":\"https://httpstatus.es/401\",\"title\":\"Unauthorized\"," +
+                            "\"status\":401,\"detail\":\"Full authentication is required to access this resource\"}"));
         } catch (final Exception e) {
             throw new AssertionError("Error occurred when calling endpoint: " + endpoint, e);
         } catch (AssertionError e) {
