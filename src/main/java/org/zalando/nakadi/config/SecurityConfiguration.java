8ffdc0a4c4b61e3b88243a35cb2e4099c233ec1d
Andrey Dyachkov <andrey.dyachkov@zalando.de>
2018-02-15 09:17:16 +0100

    aruha-602: removed mispelling in import

--- src/main/java/org/zalando/nakadi/config/SecurityConfiguration.java
+++ src/main/java/org/zalando/nakadi/config/SecurityConfiguration.java
@@ -1,6 +1,6 @@
 package org.zalando.nakadi.config;
 
-gimport org.slf4j.Logger;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
