10a19da69b3061488d450ad0ac9afd692f819f5f
Andrey Dyachkov <andrey.dyachkov@zalando.de>
2018-05-04 10:07:00 +0200

    aruha-1636: unlimited retention time by Nakadi admin

---
+++ src/main/java/org/zalando/nakadi/exceptions/runtime/EventTypeOptionsValidationException.java
@@ -0,0 +1,8 @@
+package org.zalando.nakadi.exceptions.runtime;
+
+public class EventTypeOptionsValidationException extends MyNakadiRuntimeException1 {
+
+    public EventTypeOptionsValidationException(String message) {
+        super(message);
+    }
+}
