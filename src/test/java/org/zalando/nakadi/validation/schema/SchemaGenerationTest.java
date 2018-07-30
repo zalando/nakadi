63c467a807d0c3dc0efe05d0d7c920443b8ca455
Ricardo de Cillo <ricardo.de.cillo@zalando.de>
2018-07-30 13:26:48 +0200

    Generate ksql type based on schema

--- src/test/java/org/zalando/nakadi/validation/schema/SchemaGenerationTest.java
+++ src/test/java/org/zalando/nakadi/validation/schema/SchemaGenerationTest.java
@@ -26,4 +26,62 @@ public class SchemaGenerationTest {
         }
     }
 
+    @Test
+    public void generateComplexSchema() throws Exception {
+        final JSONArray events = new JSONArray(readFile("org/zalando/nakadi/validation/spp.events.json"));
+
+        JSONObject schema = new JSONObject();
+
+        for (final Object event : events) {
+            final JSONObject eventSchema = service.schemaFor(event);
+            schema = service.mergeSchema(schema, eventSchema);
+        }
+
+        System.out.println(schema.toString());
+
+        printKSQLTableSchema(schema);
+    }
+
+    private void printKSQLTableSchema(final JSONObject schema) {
+        int counter = 1;
+        final int size = schema.getJSONObject("properties").keySet().size();
+        for (final String key : schema.getJSONObject("properties").keySet()) {
+            System.out.print(key.replace('.', '_').replace('-', '_') + " ");
+            printKSQLType(schema.getJSONObject("properties").getJSONObject(key));
+            if (counter < size) {
+                System.out.print(",");
+            }
+            counter = counter + 1;
+        }
+    }
+
+    private void printKSQLType(final JSONObject jsonObject) {
+        switch (jsonObject.getString("type")) {
+            case "string":
+                System.out.print("VARCHAR");
+                break;
+            case "number":
+                System.out.print("DOUBLE");
+                break;
+            case "boolean":
+                System.out.print("BOOLEAN");
+                break;
+            case "null":
+                System.out.print("VARCHAR");
+                break;
+            case "object":
+                System.out.print("STRUCT<");
+                printKSQLTableSchema(jsonObject);
+                System.out.print(">");
+                break;
+            case "array":
+                System.out.print("ARRAY<");
+                printKSQLType(jsonObject.getJSONObject("items"));
+                System.out.print(">");
+                break;
+            default:
+                throw new RuntimeException();
+        }
+    }
+
 }
