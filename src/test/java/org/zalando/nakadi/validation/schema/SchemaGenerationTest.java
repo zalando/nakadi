35aeffbdfac6eec79dc93f273157043ae676b53b
Ricardo de Cillo <ricardo.de.cillo@zalando.de>
2018-08-06 15:48:48 +0200

    some dirty code that we will delete
    This commit is never mean to be merged. It's here just in case I loose
    my machine.

--- src/test/java/org/zalando/nakadi/validation/schema/SchemaGenerationTest.java
+++ src/test/java/org/zalando/nakadi/validation/schema/SchemaGenerationTest.java
@@ -4,6 +4,11 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Test;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.zalando.nakadi.utils.TestUtils.readFile;
@@ -28,56 +33,125 @@ public class SchemaGenerationTest {
 
     @Test
     public void generateComplexSchema() throws Exception {
-        final JSONArray events = new JSONArray(readFile("org/zalando/nakadi/validation/spp.events.json"));
+        final JSONObject schema = new JSONObject(readFile("org/zalando/nakadi/validation/product.baseschema.json"));
 
-        JSONObject schema = new JSONObject();
+        final JSONObject expandedSchema = expandSchema(schema, schema.getJSONObject("definitions"));
 
-        for (final Object event : events) {
-            final JSONObject eventSchema = service.schemaFor(event);
-            schema = service.mergeSchema(schema, eventSchema);
-        }
+        final BufferedWriter writer = new BufferedWriter(new FileWriter("/Users/rdecillo/Desktop/expanded_schema.json"));
+        writer.write(expandedSchema.toString());
+        writer.close();
 
-        System.out.println(schema.toString());
-
-        printKSQLTableSchema(schema);
+        final BufferedWriter writerKSQL = new BufferedWriter(new FileWriter("/Users/rdecillo/Desktop/ksql_type.txt"));
+        printKSQLTableSchema(expandedSchema, writerKSQL);
+        writerKSQL.close();
     }
 
-    private void printKSQLTableSchema(final JSONObject schema) {
+    private JSONObject expandSchema(final JSONObject schema, final JSONObject definitions) {
+//        System.out.println("process: " + schema.toString());
+        if (schema.has("type")) {
+            switch (schema.getString("type")) {
+                case "string":
+                    return schema;
+                case "number":
+                    return schema;
+                case "integer":
+                    return new JSONObject().put("type", "number");
+                case "boolean":
+                    return schema;
+                case "object": {
+                    return expandObjectSchema(schema, definitions);
+                }
+                case "array": {
+                    final JSONObject newSchema = new JSONObject().put("type", "array");
+                    return newSchema.put("items", expandSchema(schema.getJSONObject("items"), definitions));
+                }
+            }
+        } else if (schema.keySet().contains("properties")) { // root level without type definition...
+            return expandObjectSchema(schema, definitions);
+        } else if (schema.keySet().contains("$ref")) {
+            return derreferenceSchema(schema, definitions);
+        } else if (schema.keySet().contains("allOf")) {
+            return mergeComposedSchema(schema, definitions, "allOf");
+        } else if (schema.keySet().contains("oneOf")) {
+            return mergeComposedSchema(schema, definitions, "oneOf");
+        } else if (schema.keySet().contains("additionalProperties")) { // TODO: additionalProperties
+            return schema;
+        }
+
+        throw new RuntimeException();
+    }
+
+    private JSONObject mergeComposedSchema(final JSONObject schema, final JSONObject definitions, final String compositionType) {
+        JSONObject returnSchema = new JSONObject().put("type", "object").put("properties", new JSONObject());
+        for (final Object aSchema : schema.getJSONArray(compositionType).toList()) {
+            returnSchema = service.mergeSchema(returnSchema, expandSchema(new JSONObject((HashMap) aSchema), definitions));
+        }
+        return returnSchema;
+    }
+
+    private JSONObject derreferenceSchema(final JSONObject schema, final JSONObject definitions) {
+        final String ref = schema.getString("$ref");
+        JSONObject finalDefinition = definitions;
+        for (final String key : ref.split("/")) {
+            if (key.equals("#") || key.equals("definitions")) {
+                continue;
+            }
+            finalDefinition = finalDefinition.getJSONObject(key);
+        }
+        return expandSchema(finalDefinition, definitions);
+    }
+
+    private JSONObject expandObjectSchema(final JSONObject schema, final JSONObject definitions) {
+        final JSONObject newSchema = new JSONObject().put("type", "object").put("properties", new JSONObject());
+        for (final String key : schema.getJSONObject("properties").keySet()) {
+            newSchema.getJSONObject("properties")
+                    .put(key, expandSchema(schema.getJSONObject("properties").getJSONObject(key), definitions));
+        }
+        return newSchema;
+    }
+
+    private void printKSQLTableSchema(final JSONObject schema, final BufferedWriter writer) throws IOException {
         int counter = 1;
         final int size = schema.getJSONObject("properties").keySet().size();
         for (final String key : schema.getJSONObject("properties").keySet()) {
-            System.out.print(key.replace('.', '_').replace('-', '_') + " ");
-            printKSQLType(schema.getJSONObject("properties").getJSONObject(key));
+            writer.write(key.replace('.', '_').replace('-', '_') + " ");
+            printKSQLType(schema.getJSONObject("properties").getJSONObject(key), writer);
             if (counter < size) {
-                System.out.print(",");
+                writer.write(",");
             }
             counter = counter + 1;
         }
     }
 
-    private void printKSQLType(final JSONObject jsonObject) {
+    private void printKSQLType(final JSONObject jsonObject, final BufferedWriter writer) throws IOException {
+        if (jsonObject.has("additionalProperties")) {
+            writer.write("MAP<VARCHAR,");
+            printKSQLType(jsonObject.getJSONObject("additionalProperties"), writer);
+            writer.write(">");
+            return;
+        }
         switch (jsonObject.getString("type")) {
             case "string":
-                System.out.print("VARCHAR");
+                writer.write("VARCHAR");
                 break;
             case "number":
-                System.out.print("DOUBLE");
+                writer.write("DOUBLE");
                 break;
             case "boolean":
-                System.out.print("BOOLEAN");
+                writer.write("BOOLEAN");
                 break;
             case "null":
-                System.out.print("VARCHAR");
+                writer.write("VARCHAR");
                 break;
             case "object":
-                System.out.print("STRUCT<");
-                printKSQLTableSchema(jsonObject);
-                System.out.print(">");
+                writer.write("STRUCT<");
+                printKSQLTableSchema(jsonObject, writer);
+                writer.write(">");
                 break;
             case "array":
-                System.out.print("ARRAY<");
-                printKSQLType(jsonObject.getJSONObject("items"));
-                System.out.print(">");
+                writer.write("ARRAY<");
+                printKSQLType(jsonObject.getJSONObject("items"), writer);
+                writer.write(">");
                 break;
             default:
                 throw new RuntimeException();
