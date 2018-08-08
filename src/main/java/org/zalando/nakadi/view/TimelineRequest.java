ce257e96474ec010032f68373ce2d29e9047182c
Ricardo de Cillo <ricardo.de.cillo@zalando.de>
2018-08-08 17:15:11 +0200

    Timeline with pre-existent topic
    This change allows the creation of a timeline with a pre defined
    topic. This is useful when the topic is created by some other tool, at
    some other point in time.
    
    One concrete example is KSQL, where the output of a query is written to
    a topic, created by KSQL. Using this feature, the operator would be able
    to expose this topic throw an event type, by creating a timeline and
    pointing to this existent topic.

--- src/main/java/org/zalando/nakadi/view/TimelineRequest.java
+++ src/main/java/org/zalando/nakadi/view/TimelineRequest.java
@@ -1,8 +1,13 @@
 package org.zalando.nakadi.view;
 
+import javax.annotation.Nullable;
+
 public class TimelineRequest {
 
     private String storageId;
+
+    @Nullable
+    private String topic;
 
     public String getStorageId() {
         return storageId;
@@ -10,5 +15,13 @@ public class TimelineRequest {
 
     public void setStorageId(final String storageId) {
         this.storageId = storageId;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(final String topic) {
+        this.topic = topic;
     }
 }
