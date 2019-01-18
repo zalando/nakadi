38579e3ddf2f3807f33fc1fafe02b3712f25aac5
vstepanov <vyacheslav.stepanov@zalando.de>
2019-01-18 12:05:37 +0100

    ARUHA-2123: removed unused imports;

--- src/test/java/org/zalando/nakadi/controller/TimelinesControllerTest.java
+++ src/test/java/org/zalando/nakadi/controller/TimelinesControllerTest.java
@@ -15,15 +15,14 @@ import org.zalando.nakadi.controller.advice.NakadiProblemExceptionHandler;
 import org.zalando.nakadi.domain.Storage;
 import org.zalando.nakadi.domain.Timeline;
 import org.zalando.nakadi.security.ClientResolver;
-import org.zalando.nakadi.service.timeline.TimelineService;
 import org.zalando.nakadi.service.FeatureToggleService;
+import org.zalando.nakadi.service.timeline.TimelineService;
 import org.zalando.nakadi.util.PrincipalMockFactory;
 import org.zalando.nakadi.utils.TestUtils;
 import org.zalando.nakadi.view.TimelineView;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static org.mockito.Mockito.when;
