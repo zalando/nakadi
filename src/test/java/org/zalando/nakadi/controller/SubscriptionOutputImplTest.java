eced4e74db44098e963d2c5b2c0e43c6a8360514
Andrey Dyachkov <andrey.dyachkov@zalando.de>
2019-04-18 17:36:33 +0200

    return 409 on request during cursor reset

---
+++ src/test/java/org/zalando/nakadi/controller/SubscriptionOutputImplTest.java
@@ -0,0 +1,37 @@
+package org.zalando.nakadi.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.zalando.nakadi.exceptions.runtime.ConflictException;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class SubscriptionOutputImplTest {
+
+    @Test
+    public void testProblemRaisedForConflictException() {
+        final SubscriptionStreamController ssc =
+                new SubscriptionStreamController(null, new ObjectMapper(), null, null, null, null, null, null);
+
+        final SubscriptionStreamController.SubscriptionOutputImpl impl =
+                ssc.new SubscriptionOutputImpl(
+                        new MockHttpServletResponse() {
+                            @Override
+                            public void setStatus(int sc) {
+                                Assert.assertEquals(HttpStatus.CONFLICT.value(), sc);
+                            }
+                        },
+                        new OutputStream() {
+                            @Override
+                            public void write(int b) throws IOException {
+                                // skip
+                            }
+                        });
+
+        impl.onException(new ConflictException("conflict during reset"));
+    }
+}
