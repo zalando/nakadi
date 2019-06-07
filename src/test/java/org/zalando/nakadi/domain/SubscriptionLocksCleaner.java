4f90759d2ea25609d85253d4cb2df19d4817abcd
vstepanov <vyacheslav.stepanov@zalando.de>
2019-06-07 11:51:58 +0200

    ARUHA-2328: improved a lot of problems;

--- src/test/java/org/zalando/nakadi/domain/SubscriptionLocksCleaner.java
+++ src/test/java/org/zalando/nakadi/domain/SubscriptionLocksCleaner.java
@@ -3,7 +3,9 @@ package org.zalando.nakadi.domain;
 import org.apache.zookeeper.ZKUtil;
 import org.apache.zookeeper.ZooKeeper;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class SubscriptionLocksCleaner {
 
@@ -14,13 +16,11 @@ public class SubscriptionLocksCleaner {
         final String contextPath = "/staging";
 
         final List<String> locks = zk.getChildren(contextPath + "/nakadi/locks", false);
-        final List<String> subscriptions = zk.getChildren(contextPath + "/nakadi/subscriptions", false);
+        final Set<String> subscriptions = new HashSet<>(zk.getChildren(contextPath + "/nakadi/subscriptions", false));
         System.out.println(locks.size() + " locks, " + subscriptions.size() + " subscriptions");
 
         int notExists = 0;
         for (int i = 0; i < locks.size(); i++) {
-            System.out.println((i * 100 / (locks.size() - 1)) + "%");
-
             String lockName = locks.get(i);
             final String subscriptionId = lockName.substring(13);
 
@@ -29,6 +29,7 @@ public class SubscriptionLocksCleaner {
                 System.out.println("Removed lock for " + subscriptionId + " as it doesn't exist");
                 notExists++;
             }
+            System.out.println(((i + 1) * 100 / locks.size()) + "%");
         }
         System.out.println("Exist: " + (locks.size() - notExists) + ", Removed: " + notExists);
     }
