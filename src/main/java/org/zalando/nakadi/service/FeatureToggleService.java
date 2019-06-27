8d9240f937c3c5ca3bec7f9da0ad459ec0c189e7
Kunal Jha <kjha@zalando-11116.corp.ad.zalando.net>
2019-06-27 16:26:54 +0200

    minor syntax fix

--- src/main/java/org/zalando/nakadi/service/FeatureToggleService.java
+++ src/main/java/org/zalando/nakadi/service/FeatureToggleService.java
@@ -40,7 +40,7 @@ public interface FeatureToggleService {
         AUDIT_LOG_COLLECTION("audit_log_collection"),
         DISABLE_DB_WRITE_OPERATIONS("disable_db_write_operations"),
         DISABLE_LOG_COMPACTION("disable_log_compaction"),
-        FORCE_EVENT_TYPE_AUTHZ("force_event_type_authz");
+        FORCE_EVENT_TYPE_AUTHZ("force_event_type_authz"),
         FORCE_SUBSCRIPTION_AUTHZ("force_subscription_authz");
 
         private final String id;
