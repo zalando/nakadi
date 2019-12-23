b476f7581ebadecbf63f0cd40fb0b19fed8ebbe3
Andrey Dyachkov <andrey.dyachkov@gmail.com>
2019-12-23 16:17:53 +0100

    change health status if instance is about to be terminated

--- src/main/java/org/zalando/nakadi/plugin/DefaultTerminationService.java
+++ src/main/java/org/zalando/nakadi/plugin/DefaultTerminationService.java
@@ -1,5 +1,7 @@
 package org.zalando.nakadi.plugin;
 
+import org.zalando.nakadi.plugin.api.exceptions.PluginException;
+
 public class DefaultTerminationService implements TerminationService {
 
     public void register(final String listenerName, final TerminationListener terminationRunnable) {
@@ -9,4 +11,9 @@ public class DefaultTerminationService implements TerminationService {
     public void deregister(final String listenerName) {
         // skip implementation for the local setup
     }
+
+    boolean isTerminating() throws PluginException {
+        return false;
+    }
+
 }
