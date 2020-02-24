e129547cc2f565dcc7159273681b165160ac3f88
Suyash Garg <suyashgargsfam@gmail.com>
2020-02-24 16:39:13 +0100

    Revert "Revert "Update Kafka Clients to 2.3.0""

--- acceptance-test/src/acceptance-test/java/org/zalando/nakadi/repository/kafka/KafkaTestHelper.java
+++ acceptance-test/src/acceptance-test/java/org/zalando/nakadi/repository/kafka/KafkaTestHelper.java
@@ -1,14 +1,16 @@
 package org.zalando.nakadi.repository.kafka;
 
-import kafka.admin.AdminUtils;
 import kafka.admin.RackAwareMode;
 import kafka.server.ConfigType;
-import kafka.utils.ZkUtils;
+import kafka.zk.AdminZkClient;
+import kafka.zk.KafkaZkClient;
+import kafka.zookeeper.ZooKeeperClient;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.utils.Time;
 import org.zalando.nakadi.view.Cursor;
 
 import java.util.List;
@@ -98,15 +100,27 @@ public class KafkaTestHelper {
     }
 
     public void createTopic(final String topic, final String zkUrl) {
-        ZkUtils zkUtils = null;
-        try {
-            zkUtils = ZkUtils.apply(zkUrl, 30000, 10000, false);
-            AdminUtils.createTopic(zkUtils, topic, 1, 1, new Properties(), RackAwareMode.Safe$.MODULE$);
-        } finally {
-            if (zkUtils != null) {
-                zkUtils.close();
-            }
+        try (KafkaZkClient zkClient = createZkClient(zkUrl)) {
+            final AdminZkClient adminZkClient = new AdminZkClient(zkClient);
+            adminZkClient.createTopic(topic, 1, 1,
+                    new Properties(), RackAwareMode.Safe$.MODULE$);
         }
+    }
+
+    private static KafkaZkClient createZkClient(final String zkUrl) {
+        return new KafkaZkClient(
+                new ZooKeeperClient(
+                        zkUrl,
+                        30000,
+                        10000,
+                        1000,
+                        Time.SYSTEM,
+                        "dummyMetricGroup",
+                        "dummyMetricType"
+                ),
+                false,
+                Time.SYSTEM
+        );
     }
 
     public static Long getTopicRetentionTime(final String topic, final String zkPath) {
@@ -118,8 +132,10 @@ public class KafkaTestHelper {
     }
 
     public static String getTopicProperty(final String topic, final String zkPath, final String propertyName) {
-        final ZkUtils zkUtils = ZkUtils.apply(zkPath, 30000, 10000, false);
-        final Properties topicConfig = AdminUtils.fetchEntityConfig(zkUtils, ConfigType.Topic(), topic);
-        return topicConfig.getProperty(propertyName);
+        try (KafkaZkClient zkClient = createZkClient(zkPath)) {
+            final AdminZkClient adminZkClient = new AdminZkClient(zkClient);
+            final Properties topicConfig = adminZkClient.fetchEntityConfig(ConfigType.Topic(), topic);
+            return topicConfig.getProperty(propertyName);
+        }
     }
 }
