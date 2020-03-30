18d238289cb8afeb621c24f3f75fbd5b212e3186
Ricardo de Cillo <ricardo.de.cillo@zalando.de>
2020-03-30 17:09:50 +0200

    Allow Nakadi admins to change compatibility mode in unsafe ways
    Users often make mistakes when editing their schemas. Sometimes it's a
    type, others it's something more serious that causes events to be
    rejected. The rules often do not allowed for such changes to be
    undone, as that would mean breaking the compatibility mode (at least
    in theory).
    
    But this has led our team to make manual changes to the database
    several times as a way to rescue users from dead ends.
    
    Since hacking the database is less than ideal, we are making it
    possible for admins to downgrade the compatibility mode, so that users
    can fix their schemas and then set the compatibility mode back to what
    it was, which they can do on their own.
    
    This is only made available for Nakadi admins, as it's a potentialy
    harmful operation if users execute this without properly reflecting on
    the downstream consequences of performing incompatible changes to schemas.

--- api-metastore/src/main/java/org/zalando/nakadi/validation/schema/CompatibilityModeChangeConstraint.java
+++ api-metastore/src/main/java/org/zalando/nakadi/validation/schema/CompatibilityModeChangeConstraint.java
@@ -5,6 +5,8 @@ import com.google.common.collect.Lists;
 import org.zalando.nakadi.domain.CompatibilityMode;
 import org.zalando.nakadi.domain.EventType;
 import org.zalando.nakadi.domain.EventTypeBase;
+import org.zalando.nakadi.plugin.api.authz.AuthorizationService;
+import org.zalando.nakadi.service.AdminService;
 
 import java.util.List;
 import java.util.Map;
@@ -17,9 +19,18 @@ public class CompatibilityModeChangeConstraint implements SchemaEvolutionConstra
             CompatibilityMode.NONE, Lists.newArrayList(CompatibilityMode.NONE, CompatibilityMode.FORWARD)
     );
 
+    private final AdminService adminService;
+
+    public CompatibilityModeChangeConstraint(final AdminService adminService) {
+        this.adminService = adminService;
+    }
+
     @Override
     public Optional<SchemaEvolutionIncompatibility> validate(final EventType original, final EventTypeBase eventType) {
-        if (!allowedChanges.get(original.getCompatibilityMode()).contains(eventType.getCompatibilityMode())) {
+        final boolean isNakadiAdmin = adminService.isAdmin(AuthorizationService.Operation.WRITE);
+        final boolean isChangeValid = allowedChanges.get(original.getCompatibilityMode())
+                .contains(eventType.getCompatibilityMode());
+        if (!isNakadiAdmin && !isChangeValid) {
             return Optional.of(new SchemaEvolutionIncompatibility("changing compatibility_mode is not allowed"));
         } else {
             return Optional.empty();
