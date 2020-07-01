711bdbcb448206b340e0486fdc2e1a12504f3faf
Thomas Abraham <thomas.abraham@zalando.de>
2020-07-01 10:56:51 +0200

    Fixing null pointer in StreamingStateTest

--- api-consumption/src/test/java/org/zalando/nakadi/service/subscription/state/StreamingStateTest.java
+++ api-consumption/src/test/java/org/zalando/nakadi/service/subscription/state/StreamingStateTest.java
@@ -158,7 +158,7 @@ public class StreamingStateTest {
         final Timeline timeline = new Timeline("t", 0, storage, "t", new Date());
         final NakadiCursor anyCursor = NakadiCursor.of(timeline, "0", "0");
 
-        when(cursorConverter.convert(any(SubscriptionCursorWithoutToken.class))).thenReturn(anyCursor);
+        when(cursorConverter.convert((SubscriptionCursorWithoutToken) any())).thenReturn(anyCursor);
         when(timelineService.getActiveTimelinesOrdered(eq("t"))).thenReturn(Collections.singletonList(timeline));
         final EventConsumer.ReassignableEventConsumer consumer = mock(EventConsumer.ReassignableEventConsumer.class);
         when(consumer.getAssignment()).thenReturn(Collections.emptySet());
@@ -208,7 +208,7 @@ public class StreamingStateTest {
 
         state.onEnter();
         final NakadiCursor anyCursor = NakadiCursor.of(timeline, "0", "0");
-        when(cursorConverter.convert(any(SubscriptionCursorWithoutToken.class))).thenReturn(anyCursor);
+        when(cursorConverter.convert((SubscriptionCursorWithoutToken)any())).thenReturn(anyCursor);
 
         state.refreshTopologyUnlocked(new Partition[]{
                 new Partition(
