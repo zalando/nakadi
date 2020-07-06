ea86b5a68d5aa31d0f2354cae3be268a93f5a901
Andrey Dyachkov <andrey.dyachkov@gmail.com>
2020-07-06 13:57:20 +0200

    scripted out nakadi start and build

---
+++ nakadi.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+function waitForNakadi() {
+  echo "Waiting for Nakadi to start up"
+  while True; do
+    res=$(curl -s -w "%{http_code}" -o /dev/null http://localhost:8080/health)
+    if ((res == 200)); then
+      echo "Nakadi is fully started"
+      return
+    fi
+    echo "Nakadi boots up"
+    sleep 5
+  done
+}
+
+function startNakadi() {
+  export SPRING_PROFILES_ACTIVE=local
+  docker-compose up -d --build
+  waitForNakadi
+}
+
+function stopNakadi() {
+  docker-compose down
+}
+
+function startStorages() {
+  docker-compose up -d postgres zookeeper kafka
+}
+
+function stopStorages() {
+  docker-compose down
+}
+
+function acceptanceTests() {
+  export SPRING_PROFILES_ACTIVE=acceptanceTest
+  docker-compose up -d --build
+  waitForNakadi
+  ./gradlew :acceptance-test:acceptanceTest
+  docker-compose down
+}
+
+function buildNakadi() {
+  ./gradlew clean
+  ./gradlew :app:bootJar
+}
+
+function help() {
+  echo "Usage: ./nakadi.sh COMMAND"
+  echo ""
+  echo "Commands:"
+  echo "  start-nakadi       build Nakadi and start docker-compose services: nakadi, postgresql, zookeeper and kafka"
+  echo "  stop-nakadi        shutdown docker-compose services"
+  echo "  start-storages     start docker-compose services: postgres, zookeeper and kafka (useful for development purposes)"
+  echo "  stop-storages      shutdown docker-compose services"
+  echo "  acceptance-tests   start Nakadi configured for acceptance tests and run acceptance tests"
+  exit 1
+}
+
+COMMAND="${1}"
+case $COMMAND in
+start-nakadi)
+  buildNakadi
+  startNakadi
+  ;;
+stop-nakadi)
+  stopNakadi
+  ;;
+start-storages)
+  startStorages
+  ;;
+stop-storages)
+  stopStorages
+  ;;
+acceptance-tests)
+  buildNakadi
+  acceptanceTests
+  ;;
+*)
+  help
+  ;;
+esac
