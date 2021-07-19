ab599716b62b7b3768dc9c7496a8129fbd315a25
Oleksandr Shulgin <oleksandr.shulgin@zalando.de>
2021-07-19 16:01:22 +0200

    fix NPE in test

--- api-misc/src/test/java/org/zalando/nakadi/controller/HealthCheckControllerTest.java
+++ api-misc/src/test/java/org/zalando/nakadi/controller/HealthCheckControllerTest.java
@@ -3,6 +3,7 @@ package org.zalando.nakadi.controller;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
+import org.zalando.nakadi.ShutdownHooks;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -10,8 +11,9 @@ public class HealthCheckControllerTest {
 
     @Test
     public void testHealthControllerReports503OnShutdown() {
+        final ShutdownHooks shutdownHooks = new ShutdownHooks();
         final AtomicBoolean shuttingDown = new AtomicBoolean(false);
-        final HealthCheckController controller = new HealthCheckController(null, shuttingDown);
+        final HealthCheckController controller = new HealthCheckController(shutdownHooks, shuttingDown);
         controller.postConstruct();
         Assert.assertEquals(
                 HttpStatus.OK,
