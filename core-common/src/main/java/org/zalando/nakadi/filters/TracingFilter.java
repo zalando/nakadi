8589137bcff75201b6290a262eda774860992d86
Oleksandr Shulgin <oleksandr.shulgin@zalando.de>
2021-08-12 16:28:51 +0200

    Revert "throw away async event listener: it's not used"

--- core-common/src/main/java/org/zalando/nakadi/filters/TracingFilter.java
+++ core-common/src/main/java/org/zalando/nakadi/filters/TracingFilter.java
@@ -10,9 +10,13 @@ import org.zalando.nakadi.plugin.api.authz.AuthorizationService;
 import org.zalando.nakadi.plugin.api.authz.Subject;
 import org.zalando.nakadi.service.TracingService;
 
+import javax.servlet.AsyncContext;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 import java.io.Closeable;
 import java.io.IOException;
@@ -28,10 +32,62 @@ public class TracingFilter extends OncePerRequestFilter {
     private static final String SPAN_CONTEXT = "span_ctx";
     private final AuthorizationService authorizationService;
 
-
     @Autowired
     public TracingFilter(final AuthorizationService authorizationService) {
         this.authorizationService = authorizationService;
+    }
+
+    private class AsyncContextTracingWrapper extends AsyncContextWrapper {
+
+        private final SpanContext referenceTracingContext;
+
+        private AsyncContextTracingWrapper(final AsyncContext context, final SpanContext referenceTracingContext) {
+            super(context);
+            this.referenceTracingContext = referenceTracingContext;
+        }
+
+        @Override
+        public void start(final Runnable runnable) {
+            this.context.start(() -> {
+                    final Span span = TracingService.buildNewFollowerSpan("async_request", referenceTracingContext)
+                            .start();
+
+                    try (Closeable ignored = TracingService.activateSpan(span)) {
+                        runnable.run();
+                    } catch (final IOException ioe) {
+                        throw new RuntimeException(ioe);
+                    } finally {
+                        traceRequest(span, (HttpServletRequest) getRequest(), (HttpServletResponse) getResponse());
+                        span.finish();
+                    }
+                });
+        }
+    }
+
+    private class AsyncRequestTracingWrapper extends HttpServletRequestWrapper {
+
+        private final HttpServletRequest request;
+        private final SpanContext referenceTracingContext;
+
+        private AsyncRequestTracingWrapper(final HttpServletRequest request,
+                                           final SpanContext referenceTracingContext) {
+            super(request);
+            this.request = request;
+            this.referenceTracingContext = referenceTracingContext;
+        }
+
+        @Override
+        public AsyncContext startAsync() throws IllegalStateException {
+            final AsyncContext asyncContext = request.startAsync();
+            return new AsyncContextTracingWrapper(asyncContext, referenceTracingContext);
+        }
+
+        @Override
+        public AsyncContext startAsync(final ServletRequest servletRequest, final ServletResponse servletResponse)
+            throws IllegalStateException {
+            final AsyncContext asyncContext = request.startAsync(servletRequest, servletResponse);
+            return new AsyncContextTracingWrapper(asyncContext, referenceTracingContext);
+        }
     }
 
     @Override
@@ -67,21 +123,30 @@ public class TracingFilter extends OncePerRequestFilter {
             span.setTag("client_id", authorizationService.getSubject().map(Subject::getName).orElse("-"));
 
             //execute request
-            filterChain.doFilter(request, response);
+            final AsyncRequestTracingWrapper requestWrapper = new AsyncRequestTracingWrapper(request, span.context());
+            filterChain.doFilter(requestWrapper, response);
 
-            response.setHeader(SPAN_CONTEXT, TracingService.getTextMapFromSpanContext(span.context()).toString());
-
-            final int statusCode = response.getStatus();
-            span.setTag("http.status_code", statusCode);
-            if (statusCode >= 500) {
-                // controllers may also set the error flag for other status codes, but we won't overwrite it here
-                TracingService.setErrorFlag();
+            if (!request.isAsyncStarted()) {
+                traceRequest(span, request, response);
             }
 
-            // content length might not be known before the request was consumed, so set it after handling
-            span.setTag("content_length", request.getContentLengthLong());
+            response.setHeader(SPAN_CONTEXT, TracingService.getTextMapFromSpanContext(span.context()).toString());
         } finally {
             span.finish();
         }
+    }
+
+    private static void traceRequest(final Span span, final HttpServletRequest request,
+                                     final HttpServletResponse response) {
+
+        final int statusCode = response.getStatus();
+        span.setTag("http.status_code", statusCode);
+        if (statusCode >= 500) {
+            // controllers may also set the error flag for other status codes, but we won't overwrite it here
+            TracingService.setErrorFlag();
+        }
+
+        // content length might not be known before the request was consumed, so set it after handling
+        span.setTag("content_length", request.getContentLengthLong());
     }
 }
