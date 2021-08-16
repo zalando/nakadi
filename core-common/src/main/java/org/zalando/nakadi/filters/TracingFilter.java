cc20f105157674bdfd78c70cc236f12f0ab48099
Oleksandr Shulgin <oleksandr.shulgin@zalando.de>
2021-08-16 09:14:53 +0200

    Move outer tracing span code to the tracing plugin
    It is too late to create the outer span here, as we need an active span in the
    auth plugin.

--- core-common/src/main/java/org/zalando/nakadi/filters/TracingFilter.java
+++ core-common/src/main/java/org/zalando/nakadi/filters/TracingFilter.java
@@ -1,8 +1,5 @@
 package org.zalando.nakadi.filters;
 
-import io.opentracing.Span;
-import io.opentracing.SpanContext;
-import io.opentracing.Tracer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -10,24 +7,14 @@ import org.zalando.nakadi.plugin.api.authz.AuthorizationService;
 import org.zalando.nakadi.plugin.api.authz.Subject;
 import org.zalando.nakadi.service.TracingService;
 
-import javax.servlet.AsyncEvent;
-import javax.servlet.AsyncListener;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.Closeable;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Component
 public class TracingFilter extends OncePerRequestFilter {
-
-    private static final String GENERIC_OPERATION_NAME = "generic_request";
-    private static final String SPAN_CONTEXT = "span_ctx";
 
     private final AuthorizationService authorizationService;
 
@@ -36,111 +23,14 @@ public class TracingFilter extends OncePerRequestFilter {
         this.authorizationService = authorizationService;
     }
 
-    private class AsyncRequestSpanFinalizer implements AsyncListener {
-
-        private final Span span;
-        private final HttpServletRequest request;
-        private final HttpServletResponse response;
-
-        private AsyncRequestSpanFinalizer(final Span span,
-                                          final HttpServletRequest request,
-                                          final HttpServletResponse response) {
-            this.span = span;
-            this.request = request;
-            this.response = response;
-        }
-
-        private void finalizeSpan() {
-            try {
-                traceRequest(span, request, response);
-            } finally {
-                span.finish();
-            }
-        }
-
-        @Override
-        public void onComplete(final AsyncEvent event) {
-            finalizeSpan();
-        }
-
-        @Override
-        public void onTimeout(final AsyncEvent event) {
-            finalizeSpan();
-        }
-
-        @Override
-        public void onError(final AsyncEvent event) {
-            finalizeSpan();
-        }
-
-        @Override
-        public void onStartAsync(final AsyncEvent event) {
-        }
-    }
-
     @Override
     protected void doFilterInternal(final HttpServletRequest request,
                                     final HttpServletResponse response,
                                     final FilterChain filterChain)
             throws IOException, ServletException {
 
-        final Tracer.SpanBuilder spanBuilder;
+        TracingService.setTag("client_id", authorizationService.getSubject().map(Subject::getName).orElse("-"));
 
-        final Map<String, String> requestHeaders = Collections.list(request.getHeaderNames())
-                .stream()
-                .collect(Collectors.toMap(h -> h, request::getHeader));
-
-        final SpanContext spanContext = TracingService.extractFromRequestHeaders(requestHeaders);
-        if (spanContext != null) {
-            spanBuilder = TracingService.buildNewChildSpan(GENERIC_OPERATION_NAME, spanContext);
-        } else {
-            spanBuilder = TracingService.buildNewSpan(GENERIC_OPERATION_NAME);
-        }
-        spanBuilder
-                .withTag("http.url", request.getRequestURI() +
-                         Optional.ofNullable(request.getQueryString()).map(q -> "?" + q).orElse(""))
-                .withTag("http.header.content_encoding",
-                         Optional.ofNullable(request.getQueryString()).map(q -> "?" + q).orElse(""))
-                .withTag("http.header.accept_encoding",
-                         Optional.ofNullable(request.getQueryString()).map(q -> "?" + q).orElse(""))
-                .withTag("http.header.user_agent",
-                         Optional.ofNullable(request.getHeader("User-Agent")).orElse("-"));
-
-        final Span span = spanBuilder.start();
-        try (Closeable ignored = TracingService.activateSpan(span)) {
-            span.setTag("client_id", authorizationService.getSubject().map(Subject::getName).orElse("-"));
-
-            filterChain.doFilter(request, response);
-        } catch (final Exception ex) {
-            TracingService.setErrorFlag(span);
-            TracingService.logError(span, ex);
-        } finally {
-            response.setHeader(SPAN_CONTEXT, TracingService.getTextMapFromSpanContext(span.context()).toString());
-
-            if (request.isAsyncStarted()) {
-                request.getAsyncContext().addListener(new AsyncRequestSpanFinalizer(span, request, response));
-            } else {
-                try {
-                    traceRequest(span, request, response);
-                } finally {
-                    span.finish();
-                }
-            }
-        }
-    }
-
-    private static void traceRequest(final Span span,
-                                     final HttpServletRequest request,
-                                     final HttpServletResponse response) {
-
-        final int statusCode = response.getStatus();
-        span.setTag("http.status_code", statusCode);
-        if (statusCode >= 500) {
-            // controllers may also set the error flag for other status codes, but we won't overwrite it here
-            TracingService.setErrorFlag();
-        }
-
-        // content length might not be known before the request was consumed, so set it after handling
-        span.setTag("content_length", request.getContentLengthLong());
+        filterChain.doFilter(request, response);
     }
 }
--- core-common/src/main/java/org/zalando/nakadi/service/TracingService.java
+++ core-common/src/main/java/org/zalando/nakadi/service/TracingService.java
@@ -6,15 +6,10 @@ import io.opentracing.Span;
 import io.opentracing.SpanContext;
 import io.opentracing.Tracer;
 import io.opentracing.tag.Tags;
-import io.opentracing.propagation.TextMapAdapter;
 import io.opentracing.util.GlobalTracer;
 
 import java.io.Closeable;
-import java.util.HashMap;
 import java.util.Map;
-
-import static io.opentracing.propagation.Format.Builtin.HTTP_HEADERS;
-import static io.opentracing.propagation.Format.Builtin.TEXT_MAP;
 
 public class TracingService {
     private static final String BUCKET_NAME_5_KB = "<5K";
@@ -35,11 +30,6 @@ public class TracingService {
 
     public static Tracer.SpanBuilder buildNewSpan(final String operationName) {
         return GlobalTracer.get().buildSpan(operationName);
-    }
-
-    public static Tracer.SpanBuilder buildNewChildSpan(final String operationName,
-                                                       final SpanContext referenceContext) {
-        return buildNewSpan(operationName).addReference(References.CHILD_OF, referenceContext);
     }
 
     public static Tracer.SpanBuilder buildNewFollowerSpan(final String operationName,
@@ -112,15 +102,5 @@ public class TracingService {
 
     public static void log(final Map<String, ?> fields) {
         getActiveSpan().log(fields);
-    }
-
-    public static SpanContext extractFromRequestHeaders(final Map<String, String> requestHeaders) {
-        return GlobalTracer.get().extract(HTTP_HEADERS, new TextMapAdapter(requestHeaders));
-    }
-
-    public static Map<String, String> getTextMapFromSpanContext(final SpanContext spanContext) {
-        final Map<String, String> textMap = new HashMap<>();
-        GlobalTracer.get().inject(spanContext, TEXT_MAP, new TextMapAdapter(textMap));
-        return textMap;
     }
 }
