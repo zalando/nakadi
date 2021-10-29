bd0cc86c646926af70f811491d7f1b8c2c432192
Oleksandr Shulgin <oleksandr.shulgin@zalando.de>
2021-10-29 15:17:26 +0200

    Add topic id and event type name to exception message
    This should come in handy when looking at the logs.

--- core-common/src/main/java/org/zalando/nakadi/exceptions/runtime/EventPublishingException.java
+++ core-common/src/main/java/org/zalando/nakadi/exceptions/runtime/EventPublishingException.java
@@ -2,11 +2,15 @@ package org.zalando.nakadi.exceptions.runtime;
 
 public class EventPublishingException extends NakadiBaseException {
 
-    public EventPublishingException(final String msg, final Exception cause) {
-        super(msg, cause);
+    public EventPublishingException(final String msg, final Exception cause, final String topicId, final String eventType) {
+        super(makeMessage(msg, topicId, eventType), cause);
     }
 
-    public EventPublishingException(final String msg) {
-        super(msg);
+    public EventPublishingException(final String msg, final String topicId, final String eventType) {
+        super(makeMessage(msg, topicId, eventType));
+    }
+
+    private static String makeMessage(final String msg, final String topicId, final String eventType) {
+        return String.format("%s: %s / %s", msg, topicId, eventType);
     }
 }
