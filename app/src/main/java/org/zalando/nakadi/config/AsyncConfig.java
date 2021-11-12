f6a9e6af3e6a2979dd033e2501bc95a8c93bf000
Oleksandr Shulgin <oleksandr.shulgin@zalando.de>
2021-11-12 16:48:28 +0100

    use cached thread pool from standard Executors

--- app/src/main/java/org/zalando/nakadi/config/AsyncConfig.java
+++ app/src/main/java/org/zalando/nakadi/config/AsyncConfig.java
@@ -1,23 +1,17 @@
 package org.zalando.nakadi.config;
 
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.AsyncTaskExecutor;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
+
+import java.util.concurrent.Executors;
 
 @Configuration
 public class AsyncConfig {
     @Bean
     @Qualifier("mvcAsyncTaskExecutor")
-    public AsyncTaskExecutor taskExecutor(
-            @Value("${webmvc.async.threadPool.coreSize:8}") final String corePoolSize,
-            @Value("${webmvc.async.threadPool.maxSize:200}") final String maxPoolSize) {
-
-        final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(Integer.valueOf(corePoolSize));
-        executor.setMaxPoolSize(Integer.valueOf(maxPoolSize));
-        return executor;
+    public AsyncTaskExecutor taskExecutor() {
+        return new ConcurrentTaskExecutor(Executors.newCachedThreadPool());
     }
 }
