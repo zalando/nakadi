d05305aa51d47a28b7f673c04a13a61afcfc6eb8
Samrat Saha <samizzy18@gmail.com>
2022-02-18 15:30:27 +0100

    Add new Avro compatibility modes

--- core-common/src/main/java/org/zalando/nakadi/domain/CompatibilityMode.java
+++ core-common/src/main/java/org/zalando/nakadi/domain/CompatibilityMode.java
@@ -1,5 +1,7 @@
 package org.zalando.nakadi.domain;
 
 public enum CompatibilityMode {
-    FORWARD, COMPATIBLE, NONE;
+    FORWARD, COMPATIBLE, NONE,
+    BACKWARD, BACKWARD_TRANSITIVE,
+    FORWARD_TRANSITIVE, COMPATIBLE_TRANSITIVE
 }
