32a6e5b68b23a4758ae6ed9e3351791a433d37cc
Thomas Abraham <thomas.abraham@zalando.de>
2022-03-09 13:38:47 +0100

    Introducing SequenceDecoder for Avro
    SequenceDecoder avoids recreating Avro parser for each event, there by
    improving the de-serialization throughput.

---
+++ core-common/src/main/java/org/zalando/nakadi/repository/kafka/AvroDeserializerWithSequenceDecoder.java
@@ -0,0 +1,42 @@
+package org.zalando.nakadi.repository.kafka;
+
+import org.apache.avro.generic.GenericRecord;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.zalando.nakadi.domain.EnvelopeHolder;
+import org.zalando.nakadi.service.AvroSchema;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class AvroDeserializerWithSequenceDecoder {
+
+    private final SequenceDecoder metadataSequenceDecoder;
+    private final SequenceDecoder eventSequenceDecoder;
+
+    public AvroDeserializerWithSequenceDecoder(final AvroSchema schemas) {
+        this.metadataSequenceDecoder = new SequenceDecoder(schemas.getMetadataSchema());
+        this.eventSequenceDecoder = new SequenceDecoder(schemas.getNakadiAccessLogSchema());
+    }
+
+    public byte[] deserializeAvro(final EnvelopeHolder envelop) throws RuntimeException {
+        try {
+            final GenericRecord metadata = metadataSequenceDecoder.read(envelop.getMetadata());
+
+            metadata.put("occurred_at", new DateTime(
+                    (long) metadata.get("occurred_at"), DateTimeZone.UTC).toString());
+            metadata.put("received_at", new DateTime(
+                    (long) metadata.get("received_at"), DateTimeZone.UTC).toString());
+
+            final GenericRecord event = eventSequenceDecoder.read(envelop.getPayload());
+
+            final StringBuilder sEvent = new StringBuilder(event.toString());
+            sEvent.deleteCharAt(sEvent.length() - 1).append(", \"metadata\":").append(metadata).append('}');
+
+            return sEvent.toString().getBytes(StandardCharsets.UTF_8);
+        } catch (final IOException io) {
+            throw new RuntimeException("failed to deserialize avro event", io);
+        }
+    }
+
+}
