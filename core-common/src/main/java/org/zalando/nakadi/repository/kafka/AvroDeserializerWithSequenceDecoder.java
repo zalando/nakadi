3223f2069a7ee5f73d1efd9963c0d6c61fbbf841
Samrat Saha <samizzy18@gmail.com>
2022-04-08 17:13:26 +0200

    filter out null values in metadata

--- core-common/src/main/java/org/zalando/nakadi/repository/kafka/AvroDeserializerWithSequenceDecoder.java
+++ core-common/src/main/java/org/zalando/nakadi/repository/kafka/AvroDeserializerWithSequenceDecoder.java
@@ -3,6 +3,7 @@ package org.zalando.nakadi.repository.kafka;
 import org.apache.avro.generic.GenericRecord;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
+import org.json.JSONObject;
 import org.zalando.nakadi.domain.EnvelopeHolder;
 import org.zalando.nakadi.service.AvroSchema;
 
@@ -40,16 +41,29 @@ public class AvroDeserializerWithSequenceDecoder {
             final SequenceDecoder eventDecoder = eventSequenceDecoders.computeIfAbsent(
                     metadata.get("schema_version").toString(),
                     (v) -> new SequenceDecoder(schemas.getEventTypeSchema(metadata.get("event_type").toString(), v)));
-
             final GenericRecord event = eventDecoder.read(envelope.getPayload());
-
             final StringBuilder sEvent = new StringBuilder(event.toString());
-            sEvent.deleteCharAt(sEvent.length() - 1).append(", \"metadata\":").append(metadata).append('}');
+
+            final var sanitizedMetadata = getJsonWithNonNullValues(metadata.toString()).toString();
+            sEvent.deleteCharAt(sEvent.length() - 1).append(", \"metadata\":").append(sanitizedMetadata).append('}');
 
             return sEvent.toString().getBytes(StandardCharsets.UTF_8);
         } catch (final IOException io) {
             throw new RuntimeException("failed to deserialize avro event", io);
         }
+    }
+
+
+    private static JSONObject getJsonWithNonNullValues(final String json){
+        final var metadataObj = new JSONObject(json);
+        final var iterator = metadataObj.keys();
+        while(iterator.hasNext()){
+            final var key = iterator.next();
+            if(metadataObj.get(key).equals(JSONObject.NULL)){
+                iterator.remove();
+            }
+        }
+        return metadataObj;
     }
 
 }
