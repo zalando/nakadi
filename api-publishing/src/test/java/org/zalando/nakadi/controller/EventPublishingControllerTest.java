013bd71b7f69d784c62436dcde0c0f68cc3d62fc
Thomas Abraham <thomas.abraham@zalando.de>
2022-05-09 14:26:33 +0200

    Fixing checkstyle mistake

--- api-publishing/src/test/java/org/zalando/nakadi/controller/EventPublishingControllerTest.java
+++ api-publishing/src/test/java/org/zalando/nakadi/controller/EventPublishingControllerTest.java
@@ -97,7 +97,8 @@ public class EventPublishingControllerTest {
         final EventPublishingController controller =
                 new EventPublishingController(publisher, Mockito.mock(BinaryEventPublisher.class),
                         eventTypeMetricRegistry, blacklistService, kpiPublisher,
-                        Mockito.mock(NakadiRecordMapper.class), Mockito.mock(PublishingResultConverter.class), "default");
+                        Mockito.mock(NakadiRecordMapper.class), Mockito.mock(PublishingResultConverter.class),
+                        "default");
 
         mockMvc = standaloneSetup(controller)
                 .setMessageConverters(new StringHttpMessageConverter(), TestUtils.JACKSON_2_HTTP_MESSAGE_CONVERTER)
