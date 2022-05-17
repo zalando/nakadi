eebe191290b0454378d0777bb4cdf80473a07419
Thomas Abraham <thomas.abraham@zalando.de>
2022-05-17 14:24:51 +0200

    Fixing acceptance tests

--- core-services/src/main/java/org/zalando/nakadi/service/publishing/NakadiKpiPublisher.java
+++ core-services/src/main/java/org/zalando/nakadi/service/publishing/NakadiKpiPublisher.java
@@ -124,6 +124,7 @@ public class NakadiKpiPublisher {
 
         final var metadata = new NakadiAvroMetadata(metadataSchemaVersion, metadataSchema);
         metadata.setOccurredAt(now);
+        metadata.setOccurredAt(now);
         metadata.setEid(uuidGenerator.randomUUID().toString());
         metadata.setEventType(eventTypeName);
         metadata.setSchemaVersion(etSchemaVersion);
