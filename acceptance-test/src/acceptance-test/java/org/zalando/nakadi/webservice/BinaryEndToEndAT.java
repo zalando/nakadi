8fbed8308f1c3ba3e0d09301ff9d8caa4e6c1fdc
Andrey Dyachkov <andrey.dyachkov@zalando.de>
2022-06-23 13:41:02 +0200

    commit cursor in test

--- acceptance-test/src/acceptance-test/java/org/zalando/nakadi/webservice/BinaryEndToEndAT.java
+++ acceptance-test/src/acceptance-test/java/org/zalando/nakadi/webservice/BinaryEndToEndAT.java
@@ -7,6 +7,7 @@ import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.generic.GenericRecordBuilder;
 import org.apache.avro.io.DecoderFactory;
 import org.apache.avro.io.EncoderFactory;
+import org.apache.http.HttpStatus;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.core.io.DefaultResourceLoader;
@@ -15,12 +16,14 @@ import org.zalando.nakadi.domain.EventCategory;
 import org.zalando.nakadi.domain.EventTypeSchema;
 import org.zalando.nakadi.domain.EventTypeSchemaBase;
 import org.zalando.nakadi.domain.Subscription;
+import org.zalando.nakadi.generated.avro.ConsumptionBatch;
 import org.zalando.nakadi.generated.avro.Envelope;
 import org.zalando.nakadi.generated.avro.Metadata;
 import org.zalando.nakadi.generated.avro.PublishingBatch;
 import org.zalando.nakadi.utils.EventTypeTestBuilder;
 import org.zalando.nakadi.utils.RandomSubscriptionBuilder;
 import org.zalando.nakadi.utils.TestUtils;
+import org.zalando.nakadi.view.SubscriptionCursor;
 import org.zalando.nakadi.webservice.utils.NakadiTestUtils;
 import org.zalando.nakadi.webservice.utils.TestStreamingClient;
 
@@ -39,7 +42,7 @@ public class BinaryEndToEndAT extends BaseAT {
     private static final String TEST_ET_NAME = TestUtils.randomValidEventTypeName();
 
     @Test
-    public void testAvroPublishingAndJsonConsumption() throws IOException {
+    public void shouldPublishAvroAndConsumeJsonAndAvro() throws IOException {
         final Schema schema = new Schema.Parser().parse(new DefaultResourceLoader()
                 .getResource("nakadi.end2end.avsc").getInputStream());
         final var et = EventTypeTestBuilder.builder()
@@ -103,7 +106,8 @@ public class BinaryEndToEndAT extends BaseAT {
         final TestStreamingClient client2 = TestStreamingClient.create(subscription2.getId()).startBinary();
 
         TestUtils.waitFor(() -> Assert.assertEquals(1, client2.getBinaryBatches().size()));
-        final Envelope binaryEvent = client2.getBinaryBatches().get(0).getEvents().get(0);
+        final ConsumptionBatch consumptionBatch = client2.getBinaryBatches().get(0);
+        final Envelope binaryEvent = consumptionBatch.getEvents().get(0);
         Assert.assertEquals("CE8C9EBC-3F19-4B9D-A453-08AD2EDA6028", binaryEvent.getMetadata().getEid());
 
         final GenericRecord genericRecord = new GenericDatumReader<GenericRecord>(schema)
@@ -111,5 +115,16 @@ public class BinaryEndToEndAT extends BaseAT {
                         .binaryDecoder(binaryEvent.getPayload().array(), null));
 
         Assert.assertEquals("bar", genericRecord.get("foo").toString());
+
+        final org.zalando.nakadi.generated.avro.SubscriptionCursor cursor = consumptionBatch.getCursor();
+        final List<SubscriptionCursor> subscriptionCursors = List.of(new SubscriptionCursor(
+                cursor.getPartition(),
+                cursor.getOffset(),
+                cursor.getEventType(),
+                cursor.getCursorToken()));
+        Assert.assertEquals(
+                HttpStatus.SC_NO_CONTENT,
+                NakadiTestUtils.commitCursors(
+                        client2.getSubscriptionId(), subscriptionCursors, client2.getSessionId()));
     }
 }
