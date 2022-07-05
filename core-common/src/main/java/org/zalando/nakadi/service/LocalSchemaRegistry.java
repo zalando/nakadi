aace4273a8a4cec3e49f616247baf5cc93dd6dcd
Thomas Abraham <thomas.abraham@zalando.de>
2022-07-05 10:03:31 +0200

    Implement SchemaProviderService in LocalSchemaRegistry

--- core-common/src/main/java/org/zalando/nakadi/service/LocalSchemaRegistry.java
+++ core-common/src/main/java/org/zalando/nakadi/service/LocalSchemaRegistry.java
@@ -1,12 +1,15 @@
 package org.zalando.nakadi.service;
 
 import org.apache.avro.Schema;
+import org.apache.avro.SchemaNormalization;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import org.zalando.nakadi.domain.EventTypeSchemaBase;
 import org.zalando.nakadi.exceptions.runtime.NoSuchEventTypeException;
 import org.zalando.nakadi.exceptions.runtime.NoSuchSchemaException;
+import org.zalando.nakadi.exceptions.runtime.UnsupportedSchemaTypeException;
 import org.zalando.nakadi.util.AvroUtils;
 
 import javax.annotation.Nullable;
@@ -22,7 +25,7 @@ import java.util.stream.Collectors;
 
 // temporarily storage for event type avro schemas untill schema repository supports them
 @Service
-public class LocalSchemaRegistry {
+public class LocalSchemaRegistry implements SchemaProviderService {
 
     public static final String ENVELOPE_KEY = "envelope";
     public static final String BATCH_PUBLISHING_KEY = "batch.publishing";
@@ -76,6 +79,7 @@ public class LocalSchemaRegistry {
         return versionToSchema;
     }
 
+    @Override
     public Schema getAvroSchema(final String schemaName, final String schemaVersion) {
         final Schema schema = getAvroSchemaVersions(schemaName).get(schemaVersion);
         if (schema == null) {
@@ -83,6 +87,28 @@ public class LocalSchemaRegistry {
                     "Avro schema not found for: " + schemaName + ", version " + schemaVersion);
         }
         return schema;
+    }
+
+    @Override
+    public String getAvroSchemaVersion(final String name, final Schema schema) throws NoSuchSchemaException {
+        // TODO maybe implement cache for this
+        final long schemaFingerprint = SchemaNormalization.parsingFingerprint64(schema);
+        return getAvroSchemaVersions(name).entrySet().stream()
+                .filter((entry) -> SchemaNormalization.parsingFingerprint64(entry.getValue()) == schemaFingerprint)
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElseThrow(() -> new NoSuchSchemaException(
+                        String.format("schema is not found for %s", name)));
+    }
+
+    @Override
+    public String getSchemaVersion(final String name, final String schema, final EventTypeSchemaBase.Type type)
+            throws NoSuchSchemaException, UnsupportedSchemaTypeException {
+        if (EventTypeSchemaBase.Type.AVRO_SCHEMA.equals(type)) {
+            return getAvroSchemaVersion(name, new Schema.Parser().parse(schema));
+        } else {
+            throw new UnsupportedSchemaTypeException("Unsupported schema type: " + type);
+        }
     }
 
     public TreeMap<String, Schema> getAvroSchemaVersions(final String schemaName) {
