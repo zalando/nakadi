84f9b01bd64c3ad1a9925e6969f6480f130b8fe3
Oleksandr Shulgin <oleksandr.shulgin@zalando.de>
2023-01-16 13:28:29 +0100

    fix

--- core-common/src/main/java/org/zalando/nakadi/repository/KafkaRepositoryCreator.java
+++ core-common/src/main/java/org/zalando/nakadi/repository/KafkaRepositoryCreator.java
@@ -64,8 +64,8 @@ public class KafkaRepositoryCreator implements TopicRepositoryCreator {
                     zookeeperSettings.getZkConnectionTimeoutMs(),
                     nakadiSettings);
             final KafkaLocationManager kafkaLocationManager = new KafkaLocationManager(zooKeeperHolder, kafkaSettings);
-            final KafkaFactory kafkaFactory =
-                    new KafkaFactory(kafkaLocationManager, nakadiSettings.getKafkaActiveProducersCount());
+            final KafkaFactory kafkaFactory = new KafkaFactory(new KafkaLocationManager(zooKeeperHolder, kafkaSettings),
+                    nakadiSettings.getKafkaActiveProducersCount());
             final KafkaZookeeper zk = new KafkaZookeeper(zooKeeperHolder, objectMapper);
             final KafkaTopicRepository kafkaTopicRepository =
                     new KafkaTopicRepository.Builder()
